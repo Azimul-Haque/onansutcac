2629647cf1e198f5734d5f22300202f0e41c45c0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-01 03:59:16 +0600

    Commit Sun 06/01/2025  3:59:16.20 6583

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -3,6 +3,16 @@
 
 @section('third_party_stylesheets')
 
+  <style>
+    .contact .map-container {
+        width: 100%;
+        height: 280px;
+        border-radius: 15px;
+        overflow: hidden;
+        box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08);
+    }
+  </style>
+
 @endsection
 
 @section('content')
