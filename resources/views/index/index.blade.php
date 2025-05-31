a341a7d8ec11fd96ca023cb8b0fcab39cdc420b5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-01 04:56:18 +0600

    Commit Sun 06/01/2025  4:56:18.24 15678

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -724,7 +724,8 @@
                     }
                   }
                 </script>
-                <div class="swiper-wrapper" id="swiper-wrapper-9c7a64342fe292c6" aria-live="off" {{-- style="transition-duration: 0ms; transform: translate3d(-744.667px, 0px, 0px); transition-delay: 0ms;" --}}>
+                <div class="swiper-wrapper" id="swiper-wrapper-9c7a64342fe292c6" aria-live="off" >
+                  {{-- style="transition-duration: 0ms; transform: translate3d(-744.667px, 0px, 0px); transition-delay: 0ms;" --}}
 
                   <div class="swiper-slide" style="width: 371.333px; margin-right: 1px;" role="group" aria-label="1 / 5" data-swiper-slide-index="0">
                     <div class="testimonial-item">
