9da34292ec623862d59b124dbc5909960d2e7387
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-01 03:49:52 +0600

    Commit Sun 06/01/2025  3:49:52.21 27904

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -565,7 +565,7 @@
       </div>
 
       <div class="text-center mt-5">
-        <a href="#" style="display: inline-block; margin-top: 15px; background-color: #205ed7; padding: 10px 20px; color: white; border-radius: 25px; font-weight: 600; text-transform: uppercase; font-size: 14px;">View All Events</a>
+        <a href="#!" style="display: inline-block; margin-top: 15px; background-color: #205ed7; padding: 10px 20px; color: white; border-radius: 25px; font-weight: 600; text-transform: uppercase; font-size: 14px;">View All Events</a>
       </div>
 
     </section><!-- /Features Section -->
