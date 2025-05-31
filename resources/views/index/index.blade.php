3ce6b96ccb49293a2f1e9169bb36752713465def
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-31 16:51:40 +0600

    Commit Sat 05/31/2025 16:51:40.24 18381

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -469,7 +469,7 @@
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Partners</h2>
+        <h2>Testimonials</h2>
         <p>Hear From Our Clients</p>
       </div><!-- End Section Title -->
       
