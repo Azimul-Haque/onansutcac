856daba97d57e95ccc577f24593a003d6d742990
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-01 03:35:48 +0600

    Commit Sun 06/01/2025  3:35:48.25 9813

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -575,7 +575,7 @@
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>In the News</h2>
-        <p>Where Our Journey Unfolds.</p>
+        <p>Where Our Journey Unfolds</p>
       </div><!-- End Section Title -->
 
       <div class="container" data-aos="fade-up">
