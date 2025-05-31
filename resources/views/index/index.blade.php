d74fafe7bdd859728bcf4250a25dc7a8c9289f39
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-31 16:33:28 +0600

    Commit Sat 05/31/2025 16:33:28.29 20903

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -373,7 +373,7 @@
 
     </section><!-- /Features Section -->
 
-    <section id="stats" class="section" style="background: url('your-background-image.jpg') no-repeat center center / cover; padding: 60px 0;">
+    <section id="stats" class="team section light-background" style="background: url('your-background-image.jpg') no-repeat center center / cover; padding: 60px 0;">
 
       <div class="container">
         <div class="row gy-4 justify-content-center">
