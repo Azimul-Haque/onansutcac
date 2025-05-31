e94fb7d9c029493e48f46819cd53f26786c53a6a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-31 22:13:05 +0600

    Commit Sat 05/31/2025 22:13:05.22 2991

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -143,7 +143,7 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item d-flex align-items-center w-100 h-100">
-              <i class="bi bi-people color-pink flex-shrink-0"></i>
+              <i class="bi bi-globe-americas color-pink flex-shrink-0"></i>
               <div>
                 <span data-purecounter-start="0" data-purecounter-end="7" data-purecounter-duration="1" class="purecounter"></span>
                 <p>Global Offices</p>
