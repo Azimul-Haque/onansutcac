c392604f073e3a3668475f6f30b887d3a73894c1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-05-31 16:47:17 +0600

    Commit Sat 05/31/2025 16:47:17.29 489

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -206,44 +206,44 @@
 
         <div class="row gy-4">
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-1.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-2.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-3.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-4.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-5.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-6.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-3.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-6.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-1.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
-          <div class="col-xl-2 col-md-3 col-6 ">
+          <div class="col-xl-2 col-md-3 col-6 client-logo">
             <img src="{{ asset('vendor/dewi/assets/img/clients/client-5.png') }}" class="img-fluid" alt="">
           </div><!-- End Client Item -->
 
