e2e096dfddcc05ee9aa5177e9c2407be5bede39f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-03 03:00:02 +0600

    Commit Tue 06/03/2025  3:00:02.28 1598

--- resources/views/index/products.blade.php
+++ resources/views/index/products.blade.php
@@ -45,10 +45,15 @@
                 <div class="icon">
                   <i class="bi bi-activity"></i>
                 </div>
-                <a href="service-details.html" class="stretched-link">
+                <a href="#!" class="stretched-link">
                   <h3>Nesciunt Mete</h3>
                 </a>
                 <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis.</p>
+
+                <a href="#" class="service-link">
+                  <span>Learn More</span>
+                  <i class="bi bi-arrow-right"></i>
+                </a>
               </div>
             </div>
           </div><!-- End Service Item -->
@@ -62,7 +67,7 @@
                 <div class="icon">
                   <i class="bi bi-broadcast"></i>
                 </div>
-                <a href="service-details.html" class="stretched-link">
+                <a href="#!" class="stretched-link">
                   <h3>Eosle Commodi</h3>
                 </a>
                 <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
@@ -79,7 +84,7 @@
                 <div class="icon">
                   <i class="bi bi-easel"></i>
                 </div>
-                <a href="service-details.html" class="stretched-link">
+                <a href="#!" class="stretched-link">
                   <h3>Ledo Markt</h3>
                 </a>
                 <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
@@ -209,7 +214,7 @@
                   <div class="icon">
                     <i class="bi bi-cash-stack" style="color: #0dcaf0;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Nesciunt Mete</h3>
                   </a>
                   <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
@@ -221,7 +226,7 @@
                   <div class="icon">
                     <i class="bi bi-calendar4-week" style="color: #fd7e14;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Eosle Commodi</h3>
                   </a>
                   <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
@@ -233,7 +238,7 @@
                   <div class="icon">
                     <i class="bi bi-chat-text" style="color: #20c997;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Ledo Markt</h3>
                   </a>
                   <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
@@ -245,7 +250,7 @@
                   <div class="icon">
                     <i class="bi bi-credit-card-2-front" style="color: #df1529;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Asperiores Commodit</h3>
                   </a>
                   <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
@@ -257,7 +262,7 @@
                   <div class="icon">
                     <i class="bi bi-globe" style="color: #6610f2;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Velit Doloremque</h3>
                   </a>
                   <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
@@ -269,7 +274,7 @@
                   <div class="icon">
                     <i class="bi bi-clock" style="color: #f3268c;"></i>
                   </div>
-                  <a href="service-details.html" class="stretched-link">
+                  <a href="#!" class="stretched-link">
                     <h3>Dolori Architecto</h3>
                   </a>
                   <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
