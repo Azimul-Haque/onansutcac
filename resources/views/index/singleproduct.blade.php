890e8b73b0d28cc300af2a5bb78789edcb982578
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-04 02:51:34 +0600

    Commit Wed 06/04/2025  2:51:34.20 7634

--- resources/views/index/singleproduct.blade.php
+++ resources/views/index/singleproduct.blade.php
@@ -119,7 +119,7 @@
       </div>
     </div>
 
-    <section id="relevant-technologies" class="py-5" style="background-image: url('YOUR_BACKGROUND_IMAGE_URL.jpg'); background-size: cover; background-position: center; min-height: 70vh; display: flex; align-items: center; justify-content: center;">
+    <section id="relevant-technologies" class="py-5 section light-background" style="background-image: url('YOUR_BACKGROUND_IMAGE_URL.jpg'); background-size: cover; background-position: center; min-height: 70vh; display: flex; align-items: center; justify-content: center;">
       <div class="container" data-aos="fade-up">
         <h2 class="text-center mb-5 text-uppercase" style="color: #6a1aed; letter-spacing: 2px;">Relevant Technologies</h2>
 
