e1fbc3fc31b143f424abcdf366112cdc2577077d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-04 03:32:24 +0600

    Commit Wed 06/04/2025  3:32:24.32 10092

--- resources/views/index/products.blade.php
+++ resources/views/index/products.blade.php
@@ -67,12 +67,12 @@
                 <div class="icon">
                   <i class="bi bi-broadcast"></i>
                 </div>
-                <a href="#!" class="stretched-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="stretched-link">
                   <h3>Eosle Commodi</h3>
                 </a>
                 <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
 
-                <a href="#" class="service-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="service-link">
                   <span>Explore More</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
@@ -89,12 +89,12 @@
                 <div class="icon">
                   <i class="bi bi-easel"></i>
                 </div>
-                <a href="#!" class="stretched-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="stretched-link">
                   <h3>Ledo Markt</h3>
                 </a>
                 <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
 
-                <a href="#" class="service-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="service-link">
                   <span>Explore More</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
@@ -111,12 +111,12 @@
                 <div class="icon">
                   <i class="bi bi-activity"></i>
                 </div>
-                <a href="#!" class="stretched-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="stretched-link">
                   <h3>Nesciunt Mete</h3>
                 </a>
                 <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis.</p>
 
-                <a href="#" class="service-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="service-link">
                   <span>Explore More</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
@@ -133,12 +133,12 @@
                 <div class="icon">
                   <i class="bi bi-broadcast"></i>
                 </div>
-                <a href="#!" class="stretched-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="stretched-link">
                   <h3>Eosle Commodi</h3>
                 </a>
                 <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
 
-                <a href="#" class="service-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="service-link">
                   <span>Explore More</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
@@ -155,12 +155,12 @@
                 <div class="icon">
                   <i class="bi bi-easel"></i>
                 </div>
-                <a href="#!" class="stretched-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="stretched-link">
                   <h3>Ledo Markt</h3>
                 </a>
                 <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
 
-                <a href="#" class="service-link">
+                <a href="{{ route('index.singleproduct', 'single') }}" class="service-link">
                   <span>Explore More</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
