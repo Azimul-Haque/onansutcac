d000c98fad9fa3d34ce804de1ecc6f7d230efcb6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-04 03:27:55 +0600

    Commit Wed 06/04/2025  3:27:55.24 6436

--- resources/views/index/singleproduct.blade.php
+++ resources/views/index/singleproduct.blade.php
@@ -329,6 +329,19 @@
               </div>
             </div><!-- End testimonial item -->
 
+            <div class="swiper-slide swiper-slide-active" style="width: 371.333px; margin-right: 1px;" role="group" aria-label="3 / 5" data-swiper-slide-index="2">
+              <div class="testimonial-item">
+                <div class="post-img">
+                  <a href="#!" target=""><img src="{{ asset('vendor/dewi/assets/img/news/blog-post-3.webp') }}" alt="" class="img-fluid"></a>
+                </div>
+
+                <h2 class="title">
+                  <a href="blog-details.html">Possimus soluta ut id suscipit ea ut in quo quia et soluta</a>
+                </h2>
+                <a href="#!" class="newsroom-item-link" target="">Read more</a>
+              </div>
+            </div><!-- End testimonial item -->
+
             
 
           </div>
