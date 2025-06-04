f467d13a669b2d3e7a9fe9f2379b1b149f8c18e4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 04:44:14 +0600

    Commit Thu 06/05/2025  4:44:14.31 2195

--- resources/views/index/markets.blade.php
+++ resources/views/index/markets.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <!-- Page Title -->
     {{-- <div class="page-title dark-background" data-aos="fade" style="background-image: url({{ asset('vendor/dewi/assets/img/page-title-bg.webp') }});"> --}}
-    <div class="page-title dark-background" data-aos="fade" style="background-image: url('https://i.makeagif.com/media/3-19-2019/pC3moX.gif');">
+    <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/market-background-top.gif') }}');">
       <div class="container position-relative">
         <h1>Markets</h1>
         <p>Discover the Industries We Empower</p>
