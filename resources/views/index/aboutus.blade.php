99d5c89ea0e144320a26ccd67120726085786a66
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 04:47:45 +0600

    Commit Thu 06/05/2025  4:47:45.23 15141

--- resources/views/index/aboutus.blade.php
+++ resources/views/index/aboutus.blade.php
@@ -325,7 +325,7 @@
     <section id="services" class="services section light-background">
 
           <!-- Section Title -->
-          <div class="container section-title aos-init aos-animate" data-aos="fade-up">
+          <div class="container section-title aos-init" data-aos="fade-up">
             <h2>Services</h2>
             <p>Our Distinct Edge</p>
           </div><!-- End Section Title -->
@@ -334,7 +334,7 @@
 
             <div class="row gy-4">
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="100">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-cash-stack" style="color: #0dcaf0;"></i>
@@ -346,7 +346,7 @@
                 </div>
               </div><!-- End Service Item -->
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="200">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-calendar4-week" style="color: #fd7e14;"></i>
@@ -358,7 +358,7 @@
                 </div>
               </div><!-- End Service Item -->
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="300">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-chat-text" style="color: #20c997;"></i>
@@ -370,7 +370,7 @@
                 </div>
               </div><!-- End Service Item -->
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="400">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-credit-card-2-front" style="color: #df1529;"></i>
@@ -382,7 +382,7 @@
                 </div>
               </div><!-- End Service Item -->
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="500">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-globe" style="color: #6610f2;"></i>
@@ -394,7 +394,7 @@
                 </div>
               </div><!-- End Service Item -->
 
-              <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="600">
+              <div class="col-lg-4 col-md-6 aos-init" data-aos="fade-up" data-aos-delay="600">
                 <div class="service-item-rifat position-relative">
                   <div class="icon">
                     <i class="bi bi-clock" style="color: #f3268c;"></i>
