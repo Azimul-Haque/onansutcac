fc913e6730ce124e7776dcf78603cb8a49dab0ab
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 03:41:20 +0600

    Commit Thu 06/05/2025  3:41:20.31 9932

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -37,11 +37,11 @@
     <h3 class="widget-title">Products</h3>
     <ul class="mt-3">
       <li><a href="#">Product - 1 <span>(25)</span></a></li>
-      <li><a href="#">Lifestyle <span>(12)</span></a></li>
-      <li><a href="#">Travel <span>(5)</span></a></li>
-      <li><a href="#">Design <span>(22)</span></a></li>
-      <li><a href="#">Creative <span>(8)</span></a></li>
-      <li><a href="#">Educaion <span>(14)</span></a></li>
+      <li><a href="#">Product - 1 <span>(25)</span></a></li>
+      <li><a href="#">Product - 1 <span>(25)</span></a></li>
+      <li><a href="#">Product - 1 <span>(25)</span></a></li>
+      <li><a href="#">Product - 1 <span>(25)</span></a></li>
+      <li><a href="#">Product - 1 <span>(25)</span></a></li>
     </ul>
 
   </div><!--/Categories Widget -->
