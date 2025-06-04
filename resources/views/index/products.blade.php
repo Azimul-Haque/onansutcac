158e1f2ce8bad75958284091e528f4d62b6529c5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 04:40:16 +0600

    Commit Thu 06/05/2025  4:40:16.24 30875

--- resources/views/index/products.blade.php
+++ resources/views/index/products.blade.php
@@ -29,7 +29,7 @@
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Products</h2>
-        <p>Tailored Solutions for Every Sector</p>
+        <p>Tailored Solutions for Every Sector<br></p>
       </div><!-- End Section Title -->
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
