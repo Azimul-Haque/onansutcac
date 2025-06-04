f4cbb7fc48156be355767aee34fe605c0139f241
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 04:32:44 +0600

    Commit Thu 06/05/2025  4:32:44.22 3369

--- resources/views/index/markets.blade.php
+++ resources/views/index/markets.blade.php
@@ -36,7 +36,7 @@
 
         <div class="row justify-content-center g-5">
 
-          <div class="col-md-6 aos-init aos-animate" data-aos="fade-right" data-aos-delay="100">
+          <div class="col-md-6 aos-init aos-animate" data-aos="fade-right" data-aos-delay="200">
             <div class="service-item">
               <div class="service-icon">
                 <i class="bi bi-code-slash"></i>
@@ -52,7 +52,7 @@
             </div>
           </div><!-- End Service Item -->
 
-          <div class="col-md-6 aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">
+          <div class="col-md-6 aos-init aos-animate" data-aos="fade-left" data-aos-delay="200">
             <div class="service-item">
               <div class="service-icon">
                 <i class="bi bi-phone-fill"></i>
