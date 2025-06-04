02fb69b41292e057572eb6d553257dc5ca39e262
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 03:49:29 +0600

    Commit Thu 06/05/2025  3:49:29.23 2070

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -20,7 +20,7 @@
         <h2 data-aos="fade-up" data-aos-delay="100">PLAN. LAUNCH. GROW.</h2>
         <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
-          <a href="#about" class="btn-get-started" style="animation: experience-float 3s ease-in-out infinite;">Get Started</a>
+          <a href="#about" class="btn-get-started">Get Started</a>
           <a href="https://www.youtube.com/watch?v=U6fC4Ij608A" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
         </div>
       </div>
