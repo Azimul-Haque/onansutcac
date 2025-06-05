cb1fb67857430233460af2df234aebffb926c072
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 13:48:07 +0600

    Commit Thu 06/05/2025 13:48:07.27 16860

--- resources/views/index/regional-offices.blade.php
+++ resources/views/index/regional-offices.blade.php
@@ -70,7 +70,7 @@
             width: 100%;
             max-width: 100%; /* Optional: constrain width on larger screens */
             height: 500px;
-            border-radius: 20px;
+            border-radius: 10px;
             background-color: white;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
             overflow: hidden;
