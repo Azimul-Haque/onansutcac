a04ec0833ef405dec70338c349703f37ffa76d43
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 17:28:59 +0600

    Commit Thu 06/05/2025 17:28:59.19 28393

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -32,7 +32,7 @@
     <h3 class="widget-title">Recent Posts</h3>
 
     <div class="post-item">
-      <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-1.webp') }}" alt="" class="flex-shrink-0">
+      <img src="assets/img/blog/blog-recent-1.jpg" alt="" class="flex-shrink-0">
       <div>
         <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
         <time datetime="2020-01-01">Jan 1, 2020</time>
@@ -40,7 +40,7 @@
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-2.webp') }}" alt="" class="flex-shrink-0">
+      <img src="assets/img/blog/blog-recent-2.jpg" alt="" class="flex-shrink-0">
       <div>
         <h4><a href="blog-details.html">Quidem autem et impedit</a></h4>
         <time datetime="2020-01-01">Jan 1, 2020</time>
@@ -48,7 +48,7 @@
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-3.webp') }}" alt="" class="flex-shrink-0">
+      <img src="assets/img/blog/blog-recent-3.jpg" alt="" class="flex-shrink-0">
       <div>
         <h4><a href="blog-details.html">Id quia et et ut maxime similique occaecati ut</a></h4>
         <time datetime="2020-01-01">Jan 1, 2020</time>
@@ -56,7 +56,7 @@
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-1.webp') }}" alt="" class="flex-shrink-0">
+      <img src="assets/img/blog/blog-recent-4.jpg" alt="" class="flex-shrink-0">
       <div>
         <h4><a href="blog-details.html">Laborum corporis quo dara net para</a></h4>
         <time datetime="2020-01-01">Jan 1, 2020</time>
@@ -64,7 +64,7 @@
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-2.webp') }}" alt="" class="flex-shrink-0">
+      <img src="assets/img/blog/blog-recent-5.jpg" alt="" class="flex-shrink-0">
       <div>
         <h4><a href="blog-details.html">Et dolores corrupti quae illo quod dolor</a></h4>
         <time datetime="2020-01-01">Jan 1, 2020</time>
@@ -94,41 +94,3 @@
   </div><!--/Tags Widget -->
 
 </div>
-
-<style>
-  .recent-posts-widget .post-item {
-    display: flex;
-    margin-bottom: 15px;
-  }
-
-  .recent-posts-widget .post-item:last-child {
-    margin-bottom: 0;
-  }
-
-  .recent-posts-widget .post-item img {
-    width: 80px;
-    margin-right: 15px;
-  }
-
-  .recent-posts-widget .post-item h4 {
-    font-size: 15px;
-    font-weight: bold;
-    margin-bottom: 5px;
-  }
-
-  .recent-posts-widget .post-item h4 a {
-    color: var(--default-color);
-    transition: 0.3s;
-  }
-
-  .recent-posts-widget .post-item h4 a:hover {
-    color: var(--accent-color);
-  }
-
-  .recent-posts-widget .post-item time {
-    display: block;
-    font-style: italic;
-    font-size: 14px;
-    color: color-mix(in srgb, var(--default-color), transparent 50%);
-  }
-</style>
