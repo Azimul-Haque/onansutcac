fdc6ecbe757b307eea5acbabaff98eb9dd53797e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 19:44:37 +0600

    Commit Thu 06/05/2025 19:44:37.23 22792

--- resources/views/index/events.blade.php
+++ resources/views/index/events.blade.php
@@ -60,6 +60,25 @@
               </div>
             </div>
 
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div class="col-lg-4 col-md-6 portfolio2-item isotope-item filter-graphics" style="position: absolute; left: 570px; top: 0px;">
               <div class="portfolio2-card">
                 <div class="portfolio2-image">
