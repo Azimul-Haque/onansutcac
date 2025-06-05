d2f6d6a74402d865b31922382c2c7e4803d22707
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 16:23:26 +0600

    Commit Thu 06/05/2025 16:23:26.26 3335

--- resources/views/index/news.blade.php
+++ resources/views/index/news.blade.php
@@ -118,10 +118,6 @@
 
         </div>
 
-        <div class="text-center mt-5">
-          <a href="#!" style="display: inline-block; margin-top: 15px; background-color: #205ed7; padding: 10px 20px; color: white; border-radius: 25px; font-weight: 600; text-transform: uppercase; font-size: 14px;">Read All News</a>
-        </div>
-
 
       </div>
 
