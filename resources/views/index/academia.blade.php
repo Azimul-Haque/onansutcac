2615c27320b46e3f2eb6e128e29bd720e147bb7a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 03:51:50 +0600

    Commit Fri 06/06/2025  3:51:50.20 22749

--- resources/views/index/academia.blade.php
+++ resources/views/index/academia.blade.php
@@ -40,7 +40,7 @@
             <div class="service-item d-flex position-relative h-100">
               <i class="bi bi-briefcase icon flex-shrink-0"></i>
               <div>
-                <h4 class="title"><a href="#" class="stretched-link">Lorem Ipsum</a></h4>
+                <h4 class="title"><a href="#" class="stretched-link">Collaboration Hub</a></h4>
                 <p class="description">CactusNano collaborates with different Universities. We work together and partner with a variety of world class institutions to fulfill NASA, NSF, DOE, and/or DOD grants.</p>
               </div>
             </div>
