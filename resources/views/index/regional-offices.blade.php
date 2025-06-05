6092bb232b5d3bd26c9fcd98522ae19f0fbc4f77
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 14:58:03 +0600

    Commit Thu 06/05/2025 14:58:03.33 13393

--- resources/views/index/regional-offices.blade.php
+++ resources/views/index/regional-offices.blade.php
@@ -158,15 +158,18 @@
               <i class="bi bi-award"></i>
               <h4>Head Office</h4>
               <p style="margin-bottom: 20px;">A108 Adam Street, New York, NY 535022</p>
+
+              <span style="font-size: 14px; font-weight: 600; color: #3b3b3b;">+44 20 7946 0958</span>
+              <span style="font-size: 14px; font-weight: 600; color: #3b3b3b;">info.uk@company.com</span>
               
               <div style="display: flex; align-items: center; margin-bottom: 4px;">
                 <i class="bi bi-telephone-fill" style="font-size: 14px; color: #d99c3b; margin-right: 6px;"></i>
-                <span style="font-size: 14px; font-weight: 600; color: #3b3b3b;">+44 20 7946 0958</span>
+                
               </div>
 
               <div style="display: flex; align-items: center;">
                 <i class="bi bi-envelope-fill" style="font-size: 14px; color: #d99c3b; margin-right: 6px;"></i>
-                <span style="font-size: 14px; font-weight: 600; color: #3b3b3b;">info.uk@company.com</span>
+                
               </div>
             </div>
           </div><!-- End Feature Borx-->
