a765060421508caeb87f41720e2981d1ab3b362e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 13:57:03 +0600

    Commit Thu 06/05/2025 13:57:03.28 6032

--- resources/views/index/regional-offices.blade.php
+++ resources/views/index/regional-offices.blade.php
@@ -58,19 +58,6 @@
               <a href="https://uk.example.com" target="_blank">Visit Site</a>
             </div>
           </div> --}}
-
-          <a href="https://example.com/usa-office" target="_blank">
-                <circle cx="420" cy="340" r="12" fill="#000">
-                  <title>USA Office</title>
-                </circle>
-              </a>
-
-              <!-- Singapore Pin -->
-              <a href="https://example.com/singapore-office" target="_blank">
-                <circle cx="1540" cy="640" r="12" fill="#000">
-                  <title>Singapore Office</title>
-                </circle>
-              </a>
         </div>
 
       </div>
