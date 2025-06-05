f8844e949569035f73ca1545c463ee642e5c40e3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 17:06:18 +0600

    Commit Thu 06/05/2025 17:06:18.27 1218

--- resources/views/index/single-news.blade.php
+++ resources/views/index/single-news.blade.php
@@ -11,7 +11,7 @@
     <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/news-page-background.gif') }}');">
       <div class="container position-relative">
         <h1>News Details</h1>
-        <p>Your Source for What's New</p>
+        <p>Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</p>
         <nav class="breadcrumbs">
           <ol>
             <li><a href="{{ route('index.index') }}">Home</a></li>
