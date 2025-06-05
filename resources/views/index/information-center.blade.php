33066cc3321cadf5e9d41e85717d32c39b1637ed
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 04:27:34 +0600

    Commit Fri 06/06/2025  4:27:34.29 18285

--- resources/views/index/information-center.blade.php
+++ resources/views/index/information-center.blade.php
@@ -35,7 +35,7 @@
       <div class="container">
 
         <div class="row gy-4 feature-boxes">
-          <div class="col-lg-4 mb-4 mb-lg-0 aos-init mt-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-4 mb-4 mb-lg-0 aos-init" data-aos="fade-up" data-aos-delay="200">
             <div class="feature-box">
               <div class="feature-icon me-sm-4 mb-3 mb-sm-0">
                 <i class="bi bi-lightbulb"></i>
