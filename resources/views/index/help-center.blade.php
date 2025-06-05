2874c0b9672b637ba95455fb5cd167569dc10215
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 05:14:17 +0600

    Commit Fri 06/06/2025  5:14:17.28 11022

--- resources/views/index/help-center.blade.php
+++ resources/views/index/help-center.blade.php
@@ -259,7 +259,7 @@
 
 @section('third_party_scripts')
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
   AOS.init(); // Initialize AOS if you're using it
@@ -292,6 +292,6 @@
       });
     });
   });
-</script>
+</script> --}}
 
 @endsection
