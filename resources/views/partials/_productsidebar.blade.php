a921b6c53c279bd40ff841346852b23383e52d9a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 17:30:00 +0600

    Commit Thu 06/05/2025 17:30:00.27 16554

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -1,6 +1,26 @@
 <div class="widgets-container">
 
-  <!-- Search Widget -->
+  {{-- <!-- Blog Author Widget -->
+  <div class="blog-author-widget widget-item">
+
+    <div class="d-flex flex-column align-items-center">
+      <img src="assets/img/blog/blog-author.jpg" class="rounded-circle flex-shrink-0" alt="">
+      <h4>Jane Smith</h4>
+      <div class="social-links">
+        <a href="https://x.com/#"><i class="bi bi-twitter-x"></i></a>
+        <a href="https://facebook.com/#"><i class="bi bi-facebook"></i></a>
+        <a href="https://instagram.com/#"><i class="biu bi-instagram"></i></a>
+        <a href="https://instagram.com/#"><i class="biu bi-linkedin"></i></a>
+      </div>
+
+      <p>
+        Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
+      </p>
+
+    </div>
+  </div><!--/Blog Author Widget --> --}}
+
+  {{-- <!-- Search Widget -->
   <div class="search-widget widget-item">
 
     <h3 class="widget-title">Search</h3>
@@ -9,19 +29,19 @@
       <button type="submit" title="Search"><i class="bi bi-search"></i></button>
     </form>
 
-  </div><!--/Search Widget -->
+  </div><!--/Search Widget --> --}}
 
   <!-- Categories Widget -->
   <div class="categories-widget widget-item">
 
-    <h3 class="widget-title">Categories</h3>
+    <h3 class="widget-title">Products</h3>
     <ul class="mt-3">
-      <li><a href="#">General <span>(25)</span></a></li>
-      <li><a href="#">Lifestyle <span>(12)</span></a></li>
-      <li><a href="#">Travel <span>(5)</span></a></li>
-      <li><a href="#">Design <span>(22)</span></a></li>
-      <li><a href="#">Creative <span>(8)</span></a></li>
-      <li><a href="#">Educaion <span>(14)</span></a></li>
+      <li><a href="#">Product - 1 <span>(USA, SG)</span></a></li>
+      <li><a href="#">Product - 2 <span>(USA)</span></a></li>
+      <li><a href="#">Product - 3 <span>(USA)</span></a></li>
+      <li><a href="#">Product - 4 <span>(USA, SG)</span></a></li>
+      <li><a href="#">Product - 5 <span>(SG)</span></a></li>
+      <li><a href="#">Product - 6 <span>(USA, SG)</span></a></li>
     </ul>
 
   </div><!--/Categories Widget -->
@@ -29,46 +49,31 @@
   <!-- Recent Posts Widget -->
   <div class="recent-posts-widget widget-item">
 
-    <h3 class="widget-title">Recent Posts</h3>
+    <h3 class="widget-title">Markets</h3>
 
     <div class="post-item">
-      <img src="assets/img/blog/blog-recent-1.jpg" alt="" class="flex-shrink-0">
-      <div>
-        <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
-        <time datetime="2020-01-01">Jan 1, 2020</time>
-      </div>
+      <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="assets/img/blog/blog-recent-2.jpg" alt="" class="flex-shrink-0">
-      <div>
-        <h4><a href="blog-details.html">Quidem autem et impedit</a></h4>
-        <time datetime="2020-01-01">Jan 1, 2020</time>
-      </div>
+      <h4><a href="blog-details.html">Quidem autem et impedit</a></h4>
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="assets/img/blog/blog-recent-3.jpg" alt="" class="flex-shrink-0">
-      <div>
-        <h4><a href="blog-details.html">Id quia et et ut maxime similique occaecati ut</a></h4>
-        <time datetime="2020-01-01">Jan 1, 2020</time>
-      </div>
+      <h4><a href="blog-details.html">Id quia et et ut maxime similique occaecati ut</a></h4>
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="assets/img/blog/blog-recent-4.jpg" alt="" class="flex-shrink-0">
-      <div>
-        <h4><a href="blog-details.html">Laborum corporis quo dara net para</a></h4>
-        <time datetime="2020-01-01">Jan 1, 2020</time>
-      </div>
+      <h4><a href="blog-details.html">Laborum corporis quo dara net para</a></h4>
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
     <div class="post-item">
-      <img src="assets/img/blog/blog-recent-5.jpg" alt="" class="flex-shrink-0">
-      <div>
-        <h4><a href="blog-details.html">Et dolores corrupti quae illo quod dolor</a></h4>
-        <time datetime="2020-01-01">Jan 1, 2020</time>
-      </div>
+      <h4><a href="blog-details.html">Et dolores corrupti quae illo quod dolor</a></h4>
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
   </div><!--/Recent Posts Widget -->
