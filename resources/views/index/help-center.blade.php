a6636411e250abdccddcbd353c1d092252249bb3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 05:06:38 +0600

    Commit Fri 06/06/2025  5:06:38.25 16122

--- resources/views/index/help-center.blade.php
+++ resources/views/index/help-center.blade.php
@@ -3,6 +3,9 @@
 
 @section('third_party_stylesheets')
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="your-custom-styles.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   
 
 @endsection
