82298120f507ee38fad16700fc1860b6814a4d62
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 14:51:43 +0600

    Commit Thu 06/05/2025 14:51:43.28 32700

--- resources/views/index/regional-offices.blade.php
+++ resources/views/index/regional-offices.blade.php
@@ -159,7 +159,7 @@
               <h4>Head Office</h4>
               <p>
                 A108 Adam Street, New York, NY 535022<br/>
-                <i class="bi bi-telephone-fill" style="font-size: 15px;"></i> <strong>+44 20 7946 0958</strong>
+                <i class="bi bi-telephone-fill" style="font-size: 15px;"></i> <strong>+44 20 7946 0958</strong><br/>
                 <i class="bi bi-envelope-fill" style="font-size: 15px;"></i> <strong>info.uk@company.com</strong>
               </p>
             </div>
