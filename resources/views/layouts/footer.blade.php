13674080b0085e59bc1f5275a0a85d7678cf1f55
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-05 16:05:24 +0600

    Commit Thu 06/05/2025 16:05:24.27 30479

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,7 +15,7 @@
         </div> --}}
         <h4>Cactus</h4>
         <ul>
-          <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="{{ {{ route('index.index') }} }}">Home</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="https://cactusmaterials.com/" target="_blank">Cactus Materials</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="https://www.cactusantimicrobial.com/" target="_blank">Cactus Antimicrobial</a></li>
