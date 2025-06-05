3b21560bee2452b0836cbaa9c8b27c30bba57f5a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 03:08:34 +0600

    Commit Fri 06/06/2025  3:08:34.23 31576

--- resources/views/index/success-stories.blade.php
+++ resources/views/index/success-stories.blade.php
@@ -33,7 +33,9 @@
 
       <div class="container aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
 
-        
+        <div class="row gy-4">
+          
+        </div>
 
         <div class="blog-posts-slider swiper init-swiper swiper-initialized swiper-horizontal swiper-backface-hidden">
           <script type="application/json" class="swiper-config">
