a75ea086cd12332152e76f4ebe545109a5da6c95
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 03:41:07 +0600

    Commit Fri 06/06/2025  3:41:07.31 12684

--- resources/views/index/single-success-story.blade.php
+++ resources/views/index/single-success-story.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <!-- Page Title -->
     {{-- <div class="page-title dark-background" data-aos="fade" style="background-image: url({{ asset('vendor/dewi/assets/img/page-title-bg.webp') }});"> --}}
-    <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/news-page-background.gif') }}');">
+    <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/other-pages-header-background.gif') }}');">
       <div class="container position-relative">
         <h1>Success Story</h1>
         <p>Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</p>
