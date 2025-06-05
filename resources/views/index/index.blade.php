1c0019532a896c4e2c7b15c24eb3d9d59c78dccf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 04:33:34 +0600

    Commit Fri 06/06/2025  4:33:34.28 22605

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1098,7 +1098,7 @@
         <a href="{{ route('index.get-contact') }}" class="btn btn-light" style="color: #39b54a; font-weight: 600; padding: 10px 24px; border-radius: 30px; animation: experience-float 3s ease-in-out infinite; margin: 5px;">
           Connect with Us
         </a>
-        <a href="{{ route('index.information-center') }}" class="btn btn-light" style="color: #39b54a; font-weight: 600; padding: 10px 24px; border-radius: 30px; animation: experience-float 3s ease-in-out infinite; margin: 5px;">
+        <a href="{{ route('index.information-center') }}" class="btn btn-dark" style="color: #39b54a; font-weight: 600; padding: 10px 24px; border-radius: 30px; animation: experience-float 3s ease-in-out infinite; margin: 5px;">
           Information Center
         </a>
       </div>
