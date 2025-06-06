f20afc994402ebce8104e5d549c20f64a1f23b97
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-06 18:04:46 +0600

    Commit Fri 06/06/2025 18:04:46.29 16309

--- resources/views/index/privacypolicy.blade.php
+++ resources/views/index/privacypolicy.blade.php
@@ -249,7 +249,7 @@
           Navigate with ease. Expertise and support are just a click away.
         </p>
         <a href="{{ route('index.get-contact') }}" class="btn btn-light" style="color: #39b54a; font-weight: 600; padding: 10px 24px; border-radius: 30px; animation: experience-float 3s ease-in-out infinite; margin: 5px;">
-          Helo Center
+          Help Center
         </a>
         <a href="{{ route('index.information-center') }}" class="btn btn-dark" style="color: #39b54a; font-weight: 600; padding: 10px 24px; border-radius: 30px; animation: experience-float 3s ease-in-out infinite; margin: 5px;">
           Information Center
