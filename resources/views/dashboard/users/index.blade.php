491c1da223fe137a04d1070e9c3322996b932412
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 01:58:40 +0600

    Commit Tue 06/10/2025  1:58:40.28 28165

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -72,7 +72,7 @@
             			      <div class="modal-dialog modal-lg" role="document">
             			        <div class="modal-content">
             			          <div class="modal-header bg-primary">
-            			            <h5 class="modal-title" id="editUserModalLabel">User তথ্য হালনাগাদ</h5>
+            			            <h5 class="modal-title" id="editUserModalLabel">Update User's Data</h5>
             			            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             			              <span aria-hidden="true">&times;</span>
             			            </button>
