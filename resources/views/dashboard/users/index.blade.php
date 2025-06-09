e19adeebcc13423d9008656cb990add1472bec38
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-09 14:26:30 +0600

    Commit Mon 06/09/2025 14:26:30.25 20645

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | ব্যবহারকারীগণ @endsection
+@section('title') Dashboard | ব্যবহারকারীগণ @endsection
 
 @section('third_party_stylesheets')
     <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" />
