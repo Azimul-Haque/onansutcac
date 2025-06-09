f54d822d1fe5a431697cdda704ccf6642513e924
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-09 14:23:10 +0600

    Commit Mon 06/09/2025 14:23:10.28 31264

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,7 +133,7 @@
     <!-- Main Footer -->
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
-            <b>ভার্শন</b> ২.০.০
+            <b>Version</b> 1.0.0
         </div>
         <strong>Copyright &copy; {{ date('Y') }} All rights
         reserved.
