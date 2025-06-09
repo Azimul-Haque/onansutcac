3061db1164370bc2f2113bacf0b671bbe946088f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 01:57:28 +0600

    Commit Tue 06/10/2025  1:57:28.28 14534

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -191,7 +191,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
-            <h5 class="modal-title" id="addUserModalLabel">নতুন User যোগ</h5>
+            <h5 class="modal-title" id="addUserModalLabel">Add New User</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
