ccf1c5d4dab88e0f9d064f06d2ffe1ecaa528433
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-09 13:18:38 +0600

    Commit Mon 06/09/2025 13:18:38.21 32077

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,8 +38,8 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function username()
-    {
-        return 'email'; // Change 'mobile_number' back to 'email'
-    }
+    // public function username()
+    // {
+    //     return 'email'; // Change 'mobile_number' back to 'email'
+    // }
 }
