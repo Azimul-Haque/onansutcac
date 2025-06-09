0683c3443cbe70ebba8b56a7ac3314f2fc985b99
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 02:01:35 +0600

    Commit Tue 06/10/2025  2:01:35.20 11962

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -130,7 +130,7 @@ class DashboardController extends Controller
     {
         $this->validate($request,array(
             'name'        => 'required|string|max:191',
-            
+            'email' => 'required|email|unique:users,email',
             'mobile'      => 'required|string|max:191|unique:users,mobile,'.$id,
             'password'    => 'nullable|string|min:8|max:191',
         ));
