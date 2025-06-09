96eee49456cbec8440433b794114e6e30cb55efd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-09 13:03:01 +0600

    Commit Mon 06/09/2025 13:03:01.28 31435

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>বিডি হেল্পলাইন | Login</title>
+    <title>CactusNano | Login</title>
 
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -18,7 +18,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>বিডি হেল্পলাইন</b></a>
+        <a href="{{ url('/home') }}"><b>CactusNano</b></a>
     </div>
     <!-- /.login-logo -->
 
