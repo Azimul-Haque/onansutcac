54df7db29264d75a813cb6f4e2dfcfb58e97b84f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-09 13:01:00 +0600

    Commit Mon 06/09/2025 13:01:00.21 20961

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('role')->default('user');
             $table->string('mobile')->unique();
-            $table->string('email')->nullable();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
