15bf59da47ea07cd0a538905047987b315f71b6b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 02:00:27 +0600

    Commit Tue 06/10/2025  2:00:27.22 31611

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -109,7 +109,7 @@ class DashboardController extends Controller
         // dd(serialize($request->sitecheck));
         $this->validate($request,array(
             'name'        => 'required|string|max:191',
-            'email' => 'required',
+            'email' => 'required|email|max:191|unique:users,mobile',
             'mobile'      => 'required|string|max:191|unique:users,mobile',
             'role'        => 'required',
             'hospitals'   => 'sometimes',
