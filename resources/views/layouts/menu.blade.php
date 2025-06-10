1b086ca3c695120154b66a99f1e1d1a068b676dd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 03:03:45 +0600

    Commit Wed 06/11/2025  3:03:45.22 31559

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,13 +6,13 @@
     </a>
 </li>
 
-@if(Auth::user()->role == 'admin')
 <li class="nav-item">
     <a href="{{ route('dashboard.users') }}" class="nav-link {{ Request::is('dashboard/users') ? 'active' : '' }} {{ Request::is('dashboard/users/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-users"></i>
         <p>Users</p>
     </a>
 </li>
+
 <li class="nav-item">
     <a href="{{ route('dashboard.products') }}" class="nav-link {{ Request::is('dashboard/products') ? 'active' : '' }} {{ Request::is('dashboard/products/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-list"></i>
