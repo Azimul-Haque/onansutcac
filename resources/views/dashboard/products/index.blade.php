3a85a1275fcbcf5f14cdd9d5f53540b6b4584ffb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 21:00:12 +0600

    Commit Tue 06/10/2025 21:00:12.30 24241

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -262,6 +262,7 @@
 @section('third_party_scripts')
     <script src="{{ asset('js/select2.full.min.js') }}"></script>
     <!-- Summernote JS for WYSIWYG editor -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
     
     <script type="text/javascript">
