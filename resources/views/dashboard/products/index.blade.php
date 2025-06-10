55263d8d1d540532a79320f61f78824079a48017
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 15:59:12 +0600

    Commit Tue 06/10/2025 15:59:12.29 28990

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -267,7 +267,7 @@
     <script type="text/javascript">
         // Ensure jQuery is loaded before attempting to initialize Summernote
         // Also ensure Bootstrap 4's JavaScript is loaded in your layouts.app
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         $(document).ready(function() {
             // Check if Summernote function exists before initializing
             if ($.fn.summernote) {
