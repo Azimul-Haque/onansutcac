7305e04aaa2353c6d8d851f2378b8fd88c4897f1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 21:43:34 +0600

    Commit Tue 06/10/2025 21:43:34.27 4474

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -235,7 +235,7 @@ class DashboardController extends Controller
         return redirect()->route('dashboard.products'); // Redirect back to the products listing
     }
 
-    public function update(Request $request, Product $product)
+    public function updateProduct(Request $request, Product $product)
     {
         $this->validate($request, [
             'title' => 'required|string|max:191',
