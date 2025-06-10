fd3f9b8acd4a49c6ca3f726e9861f3729c37edd4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 00:37:20 +0600

    Commit Wed 06/11/2025  0:37:20.31 17801

--- app/User.php
+++ app/User.php
@@ -4,65 +4,14 @@ namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    public function accessibleHospitals()
-    {
-        return $this->morphedByMany(Hospital::class, 'accessible', 'editor_access');
-    }
-
-    public function accessibleDoctors()
-    {
-        return $this->morphedByMany(Doctor::class, 'accessible', 'editor_access');
-    }
-
-    public function accessibleBlooddonors()
-    {
-        return $this->morphedByMany(Blooddonor::class, 'accessible', 'editor_access');
-    }
-
-    public function accessibleCoachings()
-    {
-        return $this->morphedByMany(Coaching::class, 'accessible', 'editor_access');
-    }
-
     public function messages(){
         return $this->hasMany('App\Message');
-    }
-
-    public function district(){
-        return $this->belongsTo('App\District');
-    }
-
-    public function accessibleTables()
-    {
-        $tables = [];
-
-        // Check hospitals
-        if ($this->accessibleHospitals()->exists()) {
-            $tables[] = 'hospitals';
-            $tables[] = 'doctors';
-        }
-
-        // Check doctors
-        if ($this->accessibleDoctors()->exists() && !in_array('doctors', $tables)) {
-            $tables[] = 'doctors'; // Doctors access only if explicitly granted
-        }
-
-        // Check blooddonors
-        if ($this->accessibleBloodDonors()->exists()) {
-            $tables[] = 'blooddonors';
-        }
-
-        // Check coachings
-        if ($this->accessibleCoachings()->exists()) {
-            $tables[] = 'coachings';
-        }
-
-        return $tables;
     }
 
     /**
