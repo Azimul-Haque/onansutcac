811ea8c052b59adadae8a4d9834fe161a5527ddd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 16:08:28 +0600

    Commit Tue 06/10/2025 16:08:28.24 8421

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,6 +140,7 @@
     </footer>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{ mix('js/app.js') }}" defer></script>
 <script src="{{ asset('js/pace.min.js') }}"></script>
 <script>
