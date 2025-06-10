ec508fd15ec0c1d72566773428dce1476b557eb4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 16:02:43 +0600

    Commit Tue 06/10/2025 16:02:43.25 14408

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -263,7 +263,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{{ asset('js/select2.full.min.js') }}"></script>
     <!-- Summernote JS for WYSIWYG editor -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
     <script type="text/javascript">
         // Ensure jQuery is loaded before attempting to initialize Summernote
