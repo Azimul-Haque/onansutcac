07c8a8050b4957c69c1f4effd65926c886875ece
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 16:01:42 +0600

    Commit Tue 06/10/2025 16:01:42.21 12214

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -263,11 +263,12 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{{ asset('js/select2.full.min.js') }}"></script>
     <!-- Summernote JS for WYSIWYG editor -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
     <script type="text/javascript">
         // Ensure jQuery is loaded before attempting to initialize Summernote
         // Also ensure Bootstrap 4's JavaScript is loaded in your layouts.app
-        // (e.g., <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>)
+   
         $(document).ready(function() {
             // Check if Summernote function exists before initializing
             if ($.fn.summernote) {
