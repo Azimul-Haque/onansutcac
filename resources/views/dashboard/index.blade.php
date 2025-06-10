e0c96e4cd5694997f46c4baed4ea085559c38a2f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 03:08:40 +0600

    Commit Wed 06/11/2025  3:08:40.22 12289

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -200,7 +200,7 @@
         </div>
         @endif
         <div class="row">
-          <div class="col-md-6">
+          {{-- <div class="col-md-6">
             <div class="card card-success">
               <div class="card-header">
                 <h3 class="card-title">ব্যবহারকারী যোগদানের হার</h3>
@@ -229,7 +229,7 @@
               </div>
               </div>
             </div>
-          </div>
+          </div> --}}
         </div>
     </div>
 @endsection
