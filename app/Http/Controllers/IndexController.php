b40008ff7a13d434197535e5441efd737fd103a0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 02:24:35 +0600

    Commit Wed 06/11/2025  2:24:35.32 2302

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -89,11 +89,7 @@ class IndexController extends Controller
 
     public function getProduct($slug)
     {
-        $product = Product::where('slug', $slug)->orderBy('id', 'desc')->first();
-        if (is_null($product)) {
-            // Option 1: Manually abort with 404
-            abort(404);
-        }
+        $product = Product::where('slug', $slug)->orderBy('id', 'desc')->firstOrFail();
 
         $markets = Market::orderBy('id', 'desc')->get()->take(6);
         $products = Product::orderBy('id', 'desc')->get()->take(6);
@@ -114,10 +110,6 @@ class IndexController extends Controller
     public function getMarket($slug)
     {
         $market = Market::where('slug', $slug)->orderBy('id', 'desc')->firstOrFail();
-        // if (is_null($market)) {
-        //     // Option 1: Manually abort with 404
-        //     abort(404);
-        // }
         
         $markets = Market::orderBy('id', 'desc')->get()->take(6);
         $products = Product::orderBy('id', 'desc')->get()->take(6);
