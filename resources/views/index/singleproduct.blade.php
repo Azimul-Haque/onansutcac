fb5e738876cfedd15d47736615ff7289d7c74324
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 23:46:28 +0600

    Commit Tue 06/10/2025 23:46:28.29 11403

--- resources/views/index/singleproduct.blade.php
+++ resources/views/index/singleproduct.blade.php
@@ -49,7 +49,7 @@
                 </div><!-- End meta top -->
 
                 <div class="content">
-                  {{ $product->text }}
+                  {!!  $product->text !!}
                   {{-- <p>
                     Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
                     Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
