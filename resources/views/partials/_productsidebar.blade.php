62666b848d7fb99cba1fa6ff8cfa01448ac51783
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 00:14:22 +0600

    Commit Wed 06/11/2025  0:14:22.26 7966

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -50,7 +50,7 @@
 
     <div class="post-item">
       <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
-      {{-- <time datetime="2020-01-01">Jan 1, 2020</time> --}}
+      <time datetime="2020-01-01">Jan 1, 2020</time>
     </div><!-- End recent post item-->
 
     <div class="post-item">
