7d870226b2ca576dc90f9642682ca04fcf546842
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 02:45:52 +0600

    Commit Wed 06/11/2025  2:45:52.24 3305

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -60,7 +60,7 @@
       <div>
         <h4><a href="{{ route('index.singleproduct', $product->slug) }}">{{ $product->title }}</a></h4>
         {{-- <time datetime="2020-01-01">{{ date('F d, Y', strtotime($product->created_at)) }}</time> --}}
-        {{ public_path('images/products/') }}
+        {{ asset('images/products/') }}
       </div>
     </div><!-- End recent post item-->
     @endforeach
