562725f89e58d159c6647a875ec2283fc5818f9f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 00:16:47 +0600

    Commit Wed 06/11/2025  0:16:47.23 14824

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -35,14 +35,15 @@
   <div class="categories-widget widget-item">
 
     <h3 class="widget-title">Products</h3>
-    {{-- <ul class="mt-3">
+    <ul class="mt-3">
       @foreach($products as $product)
         <li><a href="{{ route('index.singleproduct', $product->slug) }}">{{ $product->title }}<span>(USA, SG)</span></a></li>
       @endforeach
-    </ul> --}}
-    @foreach($products as $product)
-      <h4><a href="{{ route('index.singleproduct', $product->slug) }}">{{ $product->title }}</a></h4>
-    @endforeach
+      @foreach($products as $product)
+        <h4><a href="{{ route('index.singleproduct', $product->slug) }}">Nihil blanditiis at in nihil autem</a></h4>
+      @endforeach
+    </ul>
+
   </div><!--/Categories Widget -->
 
   <!-- Recent Posts Widget -->
