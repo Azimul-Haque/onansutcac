ebdb671e56debca13695dfa19c6dc4778d7cb4ea
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-10 21:04:57 +0600

    Commit Tue 06/10/2025 21:04:57.27 30420

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -15,6 +15,9 @@
           min-height: 200px; /* Adjust height for the editor */
       }
     </style>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
 
 @section('content')
