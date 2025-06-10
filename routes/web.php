b44932768936a0c72ca493e72d58c4d5767e8cc5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-11 00:44:26 +0600

    Commit Wed 06/11/2025  0:44:26.21 7703

--- routes/web.php
+++ routes/web.php
@@ -103,8 +103,8 @@ Route::post('/dashboard/products/store', 'DashboardController@storeProduct')->na
 Route::post('/dashboard/products/{id}/update', 'DashboardController@updateProduct')->name('dashboard.products.update');
 Route::get('/dashboard/products/{id}/delete', 'DashboardController@deleteProduct')->name('dashboard.products.delete');
 
-Route::get('/dashboard/markets', 'DashboardController@getProducts')->name('dashboard.products');
-Route::get('/dashboard/markets/{search}', 'DashboardController@getProductsSearch')->name('dashboard.products.search');
-Route::post('/dashboard/markets/store', 'DashboardController@storeProduct')->name('dashboard.products.store');
-Route::post('/dashboard/markets/{id}/update', 'DashboardController@updateProduct')->name('dashboard.products.update');
-Route::get('/dashboard/markets/{id}/delete', 'DashboardController@deleteProduct')->name('dashboard.products.delete');
+Route::get('/dashboard/markets', 'DashboardController@getProducts')->name('dashboard.markets');
+Route::get('/dashboard/markets/{search}', 'DashboardController@getProductsSearch')->name('dashboard.markets.search');
+Route::post('/dashboard/markets/store', 'DashboardController@storeProduct')->name('dashboard.markets.store');
+Route::post('/dashboard/markets/{id}/update', 'DashboardController@updateProduct')->name('dashboard.markets.update');
+Route::get('/dashboard/markets/{id}/delete', 'DashboardController@deleteProduct')->name('dashboard.markets.delete');
