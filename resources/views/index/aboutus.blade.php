0d91a5c3708b9b150efa758cefc09776255b536c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 02:17:56 +0600

    Commit Tue 06/17/2025  2:17:56.26 11851

--- resources/views/index/aboutus.blade.php
+++ resources/views/index/aboutus.blade.php
@@ -92,7 +92,7 @@
                   <h4>{{ $team->name }}</h4>
                   <span>{{ $team->designation }}</span>
                   <div class="social">
-                    <a href=""><i class="bi bi-search"></i> <small>Details</small></a>
+                    <a href="#!"><i class="bi bi-search"></i> <small>Details</small></a>
                     {{-- <a href=""><i class="bi bi-facebook"></i></a>
                     <a href=""><i class="bi bi-instagram"></i></a>
                     <a href=""><i class="bi bi-linkedin"></i></a> --}}
