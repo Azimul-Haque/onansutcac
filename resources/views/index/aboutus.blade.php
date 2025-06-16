4bfc87f8558c912086104c1b167cd193a9eecd93
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 02:31:10 +0600

    Commit Tue 06/17/2025  2:31:10.29 28304

--- resources/views/index/aboutus.blade.php
+++ resources/views/index/aboutus.blade.php
@@ -117,7 +117,7 @@
                         <div class="modal-body p-4">
                           <div class="row">
                             <div class="col-md-6">
-                              <div class="text-center ">
+                              <div class="text-center">
                                   <img id="modalTeamImage" src="{{ asset('images/teams/' . $team->image) }}" alt="Team Member" class="img-fluid rounded-circle border border-primary border-3" style="width: 200px; height: 200px; object-fit: cover; margin-bottom: 5px;">
                               </div><br/>
                               <div class="text-center ">
