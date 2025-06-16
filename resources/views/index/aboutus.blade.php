e9e6a3c908a2b04f498eece4a3dbeaf5431a7fe5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 02:05:51 +0600

    Commit Tue 06/17/2025  2:05:51.28 2077

--- resources/views/index/aboutus.blade.php
+++ resources/views/index/aboutus.blade.php
@@ -83,7 +83,7 @@
           @foreach($teams as $team)
             @php
               $modulo_index = ($loop->iteration - 1) % 3;
-              $delay = 200 + ($modulo_index * 100);
+              $delay = 100 + ($modulo_index * 100);
             @endphp
             <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
               <div class="member">
