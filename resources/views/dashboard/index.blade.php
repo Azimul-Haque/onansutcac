5eea7c7e7f51f167600b00780debfbd2af494716
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 09:47:20 +0600

    Commit Tue 06/17/2025  9:47:20.20 25589

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -131,7 +131,7 @@
           </div>
 
           <div class="col-md-3">
-              <a href="{{ route('dashboard.products') }}" class="info-box mb-3">
+              <a href="{{ route('dashboard.information-center') }}" class="info-box mb-3">
                   <span class="info-box-icon bg-info elevation-1"><i class="fas fa-info-circle"></i></span>
 
                   <div class="info-box-content">
