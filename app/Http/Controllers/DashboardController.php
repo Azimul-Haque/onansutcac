f17b1f3ca9c63d9fbbe8953631052fea477c0ea2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 10:39:09 +0600

    Commit Tue 06/17/2025 10:39:09.21 14686

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -548,8 +548,6 @@ class DashboardController extends Controller
 
     public function updateNewscategory(Request $request, $id)
     {
-        dd($request->all());
-        
         $this->validate($request, [
             'name' => 'required|string|max:191|unique:newscategories,name,' . $id,
         ]);
