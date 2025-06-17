8b8c4a2ad6db0eb002896c3dcc6a6a9a17b372f4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 10:54:27 +0600

    Commit Tue 06/17/2025 10:54:27.21 19365

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -500,7 +500,9 @@ class DashboardController extends Controller
         $news->title = $request->title;
         $news->type = $request->type;
         $news->slug = Str::slug($request->slug);
-        $news->text = Purifier::clean($request->text, 'youtube');
+        if($request->text) {
+            $news->text = Purifier::clean($request->text, 'youtube');
+        }
         if($request->text) {
             $news->text = Purifier::clean($request->text, 'youtube');
         }
