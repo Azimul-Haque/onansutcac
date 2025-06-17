25b92c6777bcae164576121ff9bba7f4f9b698c7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 09:36:18 +0600

    Commit Tue 06/17/2025  9:36:18.24 13993

--- database/migrations/2025_06_17_093154_create_newscategories_table.php
+++ database/migrations/2025_06_17_093154_create_newscategories_table.php
@@ -15,7 +15,7 @@ class CreateNewscategoriesTable extends Migration
     {
         Schema::create('newscategories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title'); 
         });
     }
 
