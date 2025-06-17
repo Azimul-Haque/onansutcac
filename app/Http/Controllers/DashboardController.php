4c867c8dc2b7fc900f2c500abf33c309403f7970
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-17 10:53:34 +0600

    Commit Tue 06/17/2025 10:53:34.21 31626

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -454,7 +454,7 @@ class DashboardController extends Controller
             'title'           => 'required|string|max:191',
             'type'            => 'nullable|string|max:191',
             'slug'            => 'required|string|max:300|unique:news,slug',
-            'newslink'        => 'sometimes|string|max:300|unique:news,slug',
+            'newslink'        => 'sometimes|string|max:300',
             'text'            => 'sometimes',
             'image'           => 'required|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
         ]);
