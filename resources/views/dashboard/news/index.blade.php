063574faa3e529206e9d7054d22b0dd65934d8a5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 16:04:12 +0600

    Commit Sat 06/21/2025 16:04:12.28 20029

--- resources/views/dashboard/news/index.blade.php
+++ resources/views/dashboard/news/index.blade.php
@@ -572,7 +572,7 @@
                 $('.text-group-' + newsId).hide();
                 $('.newslink-group-' + newsId).show();
                 $('#slugEdit' + newsId).prop('required', false);
-                $('#newsTextEdit').prop('required', false);
+                $('#newsTextEdit' + newsId).prop('required', false);
                 $('#newsLink').prop('required', true);
             } else {
                 // Show slug and text, hide link input
