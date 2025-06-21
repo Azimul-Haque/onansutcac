bb4092fb5b4f75ea584d2c73c9cac7da617741ba
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 17:16:43 +0600

    Commit Sat 06/21/2025 17:16:43.26 1337

--- resources/views/partials/_newssidebar.blade.php
+++ resources/views/partials/_newssidebar.blade.php
@@ -33,7 +33,7 @@
         <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-1.webp') }}" alt="" class="flex-shrink-0">
         <div>
           <h4><a href="{{ route('index.single-news', 'slug') }}">{{ $news->title }}</a></h4>
-          <time datetime="{{ $news->created_at }}">{{ date('F d, Y', strtotime($news)) }}</time>
+          <time datetime="{{ $news->created_at }}">{{ date('F d, Y', strtotime($news->created_at)) }}</time>
         </div>
       </div>
     @endforeach
