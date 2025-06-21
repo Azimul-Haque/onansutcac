40c6e90fdde1ac692335128ba2e0ce3c686df2fc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 15:43:23 +0600

    Commit Sat 06/21/2025 15:43:23.22 28665

--- resources/views/dashboard/news/index.blade.php
+++ resources/views/dashboard/news/index.blade.php
@@ -61,7 +61,7 @@
                                             <small class="text-black-50">{{ $news->newscategory->name ?? 'N/A' }}</small>
                                         </td>
                                         <td>{{ news_type($news->type) }}</td>
-                                        <td width="25%">
+                                        <td width="20%">
                                             @if($news->type == 3)
                                                 <small>{{ $news->newslink }}</small>
                                             @else
