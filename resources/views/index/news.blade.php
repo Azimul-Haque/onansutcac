7ddc136f4015fe433ec2c68dca813891ef5ea114
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 16:45:30 +0600

    Commit Sat 06/21/2025 16:45:30.25 30168

--- resources/views/index/news.blade.php
+++ resources/views/index/news.blade.php
@@ -79,7 +79,7 @@
       <div class="container">
         <div class="d-flex justify-content-center">
           {{ $allNews->links() }}
-          <ul>
+          {{-- <ul>
             <li><a href="#"><i class="bi bi-chevron-left"></i></a></li>
             <li><a href="#">1</a></li>
             <li><a href="#" class="active">2</a></li>
@@ -88,7 +88,7 @@
             <li>...</li>
             <li><a href="#">10</a></li>
             <li><a href="#"><i class="bi bi-chevron-right"></i></a></li>
-          </ul>
+          </ul> --}}
         </div>
       </div>
 
