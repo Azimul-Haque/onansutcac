66ef74cbe024cdd7e8b38ec4fc076ce552168b44
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 17:19:16 +0600

    Commit Sat 06/21/2025 17:19:16.25 14504

--- resources/views/partials/_newssidebar.blade.php
+++ resources/views/partials/_newssidebar.blade.php
@@ -32,7 +32,7 @@
       <div class="post-item">
         <img src="{{ asset('images/news/' . $news->image) }}" alt="{{ $news->title }}" class="flex-shrink-0">
         <div>
-          <h4><a href="{{ route('index.single-news', $news->slug) }}">{{ $news->title }}</a></h4>
+          <h4><a href="@if($news->type == 3) {{ $news->newslink }} @else {{ route('index.single-news', $news->slug) }} @endif" @if($news->type == 3) target="_blank" @endif>{{ $news->title }}</a></h4>
           <time datetime="{{ $news->created_at }}">{{ date('F d, Y', strtotime($news->created_at)) }}</time>
         </div>
       </div>
