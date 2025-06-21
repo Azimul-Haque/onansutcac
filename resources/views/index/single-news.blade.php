e6e29dc4a9f5bb2d2a2d1a8d013ef3e216a05fdd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 16:55:03 +0600

    Commit Sat 06/21/2025 16:55:03.20 17225

--- resources/views/index/single-news.blade.php
+++ resources/views/index/single-news.blade.php
@@ -34,7 +34,7 @@
               <article class="article">
 
                 <div class="post-img">
-                  <img src="{{ asset('vendor/dewi/assets/img/news/blog-post-2.webp') }}" alt="" class="img-fluid">
+                  <img src="{{ asset('images/news/' . $news->image) }}" alt="" class="img-fluid">
                 </div>
 
                 <h2 class="title">{{ $news->title }}</h2>
