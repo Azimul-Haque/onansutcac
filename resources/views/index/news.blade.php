11252252cbed4101bd4df5e55c4e7f30358dd112
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 16:43:19 +0600

    Commit Sat 06/21/2025 16:43:19.25 18104

--- resources/views/index/news.blade.php
+++ resources/views/index/news.blade.php
@@ -35,6 +35,39 @@
       <div class="container" data-aos="fade-up">
 
         <div class="row gy-4">
+
+          @foreach($newsforhomepage as $news)
+            @php
+              $modulo_index = ($loop->iteration - 1) % 3;
+              $delay = 200 + ($modulo_index * 100);
+            @endphp
+            <div class="col-xl-4 col-md-6 aos-init" data-aos="zoom-in" data-aos-delay="{{ $delay }}">
+              <article>
+
+                <div class="post-img">
+                  <a href="@if($news->type == 3) {{ $news->newslink }} @else {{ route('index.single-news', $news->slug) }} @endif" @if($news->type == 3) target="_blank" @endif><img src="{{ asset('images/news/' . $news->image) }}" alt="" class="img-fluid"></a>
+                </div>
+
+                <p class="post-category">{{ $news->newscategory->name }}</p>
+
+                <h2 class="title">
+                  <a href="@if($news->type == 3) {{ $news->newslink }} @else {{ route('index.single-news', $news->slug) }} @endif" @if($news->type == 3) target="_blank" @endif>{{ $news->title }}</a>
+                </h2>
+
+                <div class="d-flex align-items-center">
+                  <div class="post-meta">
+                    <p class="post-author">{{ news_type($news->type) }}</p>
+                    <p class="post-date">
+                      <time datetime="2022-01-01">{{ date('F d, Y', strtotime($news->created_at)) }}</time>
+                    </p>
+                  </div>
+                </div>
+                <a href="@if($news->type == 3) {{ $news->newslink }} @else {{ route('index.single-news', $news->slug) }} @endif" class="newsroom-item-link" @if($news->type == 3) target="_blank" @endif>Read more</a>
+
+              </article>
+            </div>
+          @endforeach
+          
           <div class="col-xl-4 col-md-6 aos-init" data-aos="zoom-in" data-aos-delay="200">
             <article>
 
