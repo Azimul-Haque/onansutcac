52b773e9cf984e3157b3389af84e9b70af694af3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 17:00:55 +0600

    Commit Sat 06/21/2025 17:00:55.25 16660

--- resources/views/index/single-news.blade.php
+++ resources/views/index/single-news.blade.php
@@ -41,7 +41,7 @@
 
                 <div class="meta-top">
                   <ul>
-                    <li class="d-flex align-items-center"><i class="bi bi-megaphone"></i><a href="" title="">{{ news_type($news->type) }}</a></li>
+                    <li class="d-flex align-items-center"><i class="bi bi-megaphone"></i><a href="#!">{{ news_type($news->type) }}</a></li>
                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Jan 1, 2022</time></a></li>
                     {{-- <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-details.html">12 Comments</a></li> --}}
                   </ul>
