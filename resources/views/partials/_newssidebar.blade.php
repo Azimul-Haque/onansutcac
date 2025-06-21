9e381e6bfa0f11e345aab3d53102d1c4cfd5bae6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-21 17:13:45 +0600

    Commit Sat 06/21/2025 17:13:45.22 3351

--- resources/views/partials/_newssidebar.blade.php
+++ resources/views/partials/_newssidebar.blade.php
@@ -19,11 +19,6 @@
       @foreach($newscategories as $newscategory)
         <li><a href="#">{{ $newscategory->name }} <span>({{ $newscategory->news->count() }})</span></a></li>
       @endforeach
-      <li><a href="#">Lifestyle <span>(12)</span></a></li>
-      <li><a href="#">Travel <span>(5)</span></a></li>
-      <li><a href="#">Design <span>(22)</span></a></li>
-      <li><a href="#">Creative <span>(8)</span></a></li>
-      <li><a href="#">Educaion <span>(14)</span></a></li>
     </ul>
 
   </div><!--/Categories Widget -->
