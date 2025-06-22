713e7b56c6193d022b45bdb60cbc07d6d087add9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-23 01:52:04 +0600

    Commit Mon 06/23/2025  1:52:04.31 30493

--- resources/views/index/single-news.blade.php
+++ resources/views/index/single-news.blade.php
@@ -57,12 +57,12 @@
                     <li><a href="{{ route('index.categories.news', Str::slug($news->newscategory->name)) }}" class="badge bg-success" style="color: #FFFFFF;">{{ $news->newscategory->name }}</a></li>
                   </ul>
 
-                  {{-- <i class="bi bi-tags"></i> --}}
+                  {{-- <i class="bi bi-tags"></i>
                   <ul class="tags">
-                    {{-- <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Creative</a></li>
+                    <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Creative</a></li>
                     <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Tips</a></li>
-                    <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Marketing</a></li> --}}
-                  </ul>
+                    <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Marketing</a></li>
+                  </ul> --}}
                 </div><!-- End meta bottom -->
 
               </article>
