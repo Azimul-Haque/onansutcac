53a6bb09441924e7ead341ce86a275ae068592b5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-23 02:18:48 +0600

    Commit Mon 06/23/2025  2:18:48.23 18306

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -150,10 +150,10 @@
         $pill_type = "primary";
       } elseif($type == 2) {
         $text = "Technical";
-        $pill_type = "primary";
+        $pill_type = "success";
       } elseif($type == 3) {
         $text = "Support";
-        $pill_type = "primary";
+        $pill_type = "warning";
       }
       return $text;
   }
