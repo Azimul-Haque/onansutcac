c528dbe99c09658926f0ef9a747f4be409a4db58
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-23 01:56:00 +0600

    Commit Mon 06/23/2025  1:56:00.27 23099

--- database/migrations/2025_06_23_015509_create_faqs_table.php
+++ database/migrations/2025_06_23_015509_create_faqs_table.php
@@ -16,8 +16,8 @@ class CreateFaqsTable extends Migration
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
             $table->integer('type')->unsigned();             
-            $table->string('question');             
-            $table->string('type');             
+            $table->text('question');             
+            $table->text('answer');             
             $table->timestamps();
         });
     }
