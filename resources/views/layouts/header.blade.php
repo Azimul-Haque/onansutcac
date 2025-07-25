c38ac58bf45d21cca249060483a08f03030fd661
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 15:27:20 +0600

    Commit Fri 07/25/2025 15:27:20.25 3049

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,8 +9,6 @@
 
     <nav id="navmenu" class="navmenu">
       <ul>
-
-        {{-- <li><a href="#hero" class="active">Home</a></li> --}}
         <li class="dropdown"><a href="#"><span>Company</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
           <ul>
             <li><a href="#">USA</a></li>
