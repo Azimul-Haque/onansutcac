b666574a682ad8e342c5340595a822f3feefc5c0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 15:49:00 +0600

    Commit Fri 07/25/2025 15:49:00.30 28815

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -61,7 +61,7 @@
 
         <li class="dropdown"><a href="#"><span>Sustainability</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
           <ul>
-            <li><a href="{{ route('index.get-about-us') }}" class="{{ Request::is('about-us') ? 'active' : '' }}">About Us</a></li>
+            <li><a href="{{ route('index.get-contact') }}" class="{{ Request::is('contact') ? 'active' : '' }}">About Us</a></li>
           </ul>
         </li>
 
