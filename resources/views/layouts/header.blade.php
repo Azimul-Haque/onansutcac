35614dc475e3464c9ab13b2eba2d11d94cad4c85
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 21:35:55 +0600

    Commit Fri 07/25/2025 21:35:55.26 9873

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,7 +17,7 @@
             <li><a href="{{ route('index.get-about-us') }}#team">Leadership Team</a></li>
             <li><a href="{{ route('index.get-why-work-with-us') }}">Why work with us</a></li>
             <li><a href="{{ route('index.get-why-work-with-us') }}#product-capacity">Production Capacity</a></li>
-            <li><a href="{{ route('index.get-why-work-with-us') }}#product-capacity">Global Presence</a></li>
+            <li><a href="{{ route('index.get-why-work-with-us') }}#global-presence">Global Presence</a></li>
           </ul>
         </li>
 
