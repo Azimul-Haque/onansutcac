6860b880ca0cbad4300b42a4dee8ab66a65907ea
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 15:29:06 +0600

    Commit Fri 07/25/2025 15:29:06.24 4146

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,13 +12,13 @@
         <li class="dropdown"><a href="#"><span>Company</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
           <ul>
             <li><a href="{{ route('index.get-about-us') }}" class="{{ Request::is('about-us') ? 'active' : '' }}">About Us</a></li>
-            <li><a href="{{ route('index.success-stories') }}" class="{{ Request::is('about-us') ? 'active' : '' }}">About Us</a></li>
+            <li><a href="{{ route('index.success-stories') }}" class="{{ Request::is('success-stories') ? 'active' : '' }}">Our Story</a></li>
           </ul>
         </li>
 
 
         
-        • Our Story
+        • 
         • Vision & Mission
         • Leadership Team
         • Why work with us
