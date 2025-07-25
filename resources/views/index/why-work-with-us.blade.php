6c0ac4ef45428d1ffe743163b094e543b8d7393e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 20:33:51 +0600

    Commit Fri 07/25/2025 20:33:51.22 25356

--- resources/views/index/why-work-with-us.blade.php
+++ resources/views/index/why-work-with-us.blade.php
@@ -36,7 +36,7 @@
           <!-- Box 1: Slow, controlled release -->
           <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
               <div class="feature-box">
-                  <i class="fas fa-lightbulb icon"></i>
+                  <i class="bi bi-lightbulb icon"></i>
                   <p class="text">Slow, controlled release of antimicrobial agents for long-lasting performance</p>
               </div>
           </div>
@@ -44,7 +44,7 @@
           <!-- Box 2: Initially developed for NASA -->
           <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
               <div class="feature-box">
-                  <i class="fas fa-award icon"></i>
+                  <i class="bi bi-award icon"></i>
                   <p class="text">Initially developed for NASA, now available for mass market applications</p>
               </div>
           </div>
@@ -52,7 +52,7 @@
           <!-- Box 3: Customizable & easy integration -->
           <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
               <div class="feature-box">
-                  <i class="fas fa-puzzle-piece icon"></i>
+                  <i class="bi bi-puzzle icon"></i>
                   <p class="text">Customizable & easy integration into a wide range of materials</p>
               </div>
           </div>
@@ -60,7 +60,7 @@
           <!-- Box 4: Expert technical and customer support -->
           <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
               <div class="feature-box">
-                  <i class="fas fa-star icon"></i>
+                  <i class="bi bi-star icon"></i>
                   <p class="text">Expert technical and customer support to ensure customer satisfaction</p>
               </div>
           </div>
