9d86475fccb8b2e359adac9c3875aaebe77f44a9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 15:36:36 +0600

    Commit Fri 07/25/2025 15:36:36.24 18330

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,12 +31,12 @@
 
         <li class="dropdown"><a href="#"><span>Solution Products</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
           <ul>
-            <li><a href="#!">CoreSil™ Nanomaterials</a></li>
+            <li><a href="#!">Fouling & Biofouling Control</a></li>
             <li><a href="#!">Cera2D™ Corrosion Inhibitor</a></li>
             <li><a href="#!">EndPFAS™ Solution</a></li>
           </ul>
         </li>
-        
+
         <li><a href="{{ route('index.products') }}" class="{{ Request::is('products') || Request::is('products/*') ? 'active' : '' }}">Products</a></li>
         <li><a href="{{ route('index.markets') }}" class="{{ Request::is('markets') || Request::is('markets/*') ? 'active' : '' }}">Markets</a></li>
         
