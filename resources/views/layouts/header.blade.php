805abdf1b4f07b973cec837c35c9b6f982b9cbf1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-25 15:51:52 +0600

    Commit Fri 07/25/2025 15:51:52.27 20360

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -80,8 +80,6 @@
         <li><a href="{{ route('index.markets') }}" class="{{ Request::is('markets') || Request::is('markets/*') ? 'active' : '' }}">Markets</a></li> --}}
         
         
-        {{-- <li><a href="{{ route('index.news') }}" class="{{ Request::is('news') || Request::is('news/*') ? 'active' : '' }}">News</a></li> --}}
-        
       </ul>
       <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
     </nav>
