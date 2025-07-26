2f57b241254a2d2a1968449c357b803239ee8a4b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 19:27:10 +0600

    Commit Sat 07/26/2025 19:27:10.26 16897

--- database/migrations/2025_07_26_192500_create_events_table.php
+++ database/migrations/2025_07_26_192500_create_events_table.php
@@ -17,6 +17,8 @@ class CreateEventsTable extends Migration
             $table->id();                              
             $table->string('title');         
             $table->string('type');             
+            $table->string('from_to');             
+            $table->string('address');             
             $table->text('reg_url')->nullable();                 
             $table->longText('text')->nullable();      
             $table->string('image')->nullable();           
