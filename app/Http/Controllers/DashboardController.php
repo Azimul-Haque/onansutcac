0fe3ba6913173c46af73a0a84531225afd21d766
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 23:00:06 +0600

    Commit Sat 07/26/2025 23:00:06.25 32411

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -772,6 +772,7 @@ class DashboardController extends Controller
         if ($request->hasFile('file')) {
             $fileName = time() . '_file.' . $request->file('file')->extension();
             $request->file('file')->public_path('files/success-stories', $fileName);
+            $successstory->image = $imageName;
         }
 
         Successstory::create([
