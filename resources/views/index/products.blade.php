d6467a2ccbb3bebce01abadc966813eb043b8fd1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 17:57:45 +0600

    Commit Sat 07/26/2025 17:57:45.30 26379

--- resources/views/index/products.blade.php
+++ resources/views/index/products.blade.php
@@ -10,7 +10,7 @@
     {{-- <div class="page-title dark-background" data-aos="fade" style="background-image: url({{ asset('vendor/dewi/assets/img/page-title-bg.webp') }});"> --}}
     <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/other-pages-header-background.gif') }}');">
       <div class="container position-relative">
-        <h1>Products</h1>
+        <h1>Products/Technologies</h1>
         <p>Discover Solutions Designed for You</p>
         <nav class="breadcrumbs">
           <ol>
