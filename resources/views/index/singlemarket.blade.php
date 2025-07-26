10be102e94260156122019a345376414717a4bde
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 18:36:11 +0600

    Commit Sat 07/26/2025 18:36:11.22 6706

--- resources/views/index/singlemarket.blade.php
+++ resources/views/index/singlemarket.blade.php
@@ -105,7 +105,7 @@
             <div class="col-md-4">
               <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 @foreach($marketsforfooter as $market)
-                  <a class="nav-link custom-nav-link active" id="v-pills-tab-{{ $market->id }}" data-bs-toggle="pill" data-bs-target="#v-pills-{{ $market->id }}" type="button" role="tab" aria-controls="v-pills-{{ $market->id }}" aria-selected="true">Market - 1</a>
+                  <a class="nav-link custom-nav-link active" id="v-pills-tab-{{ $market->id }}" data-bs-toggle="pill" data-bs-target="#v-pills-{{ $market->id }}" type="button" role="tab" aria-controls="v-pills-{{ $market->id }}" aria-selected="true">{{ $market->title }}</a>
                 @endforeach
                 
                 <a class="nav-link custom-nav-link" id="v-pills-bio-tab" data-bs-toggle="pill" data-bs-target="#v-pills-bio" type="button" role="tab" aria-controls="v-pills-bio" aria-selected="false">Market - 2</a>
