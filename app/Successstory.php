75bedfed87a86cafb3ee80eafa2c5f96fa74161c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 22:55:29 +0600

    Commit Sat 07/26/2025 22:55:29.28 31622

--- app/Successstory.php
+++ app/Successstory.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Successstory extends Model
 {
-    //
+    protected $fillable = [
+        'title', // Add 'title' here
+        // Other fillable attributes if any
+    ];
 }
