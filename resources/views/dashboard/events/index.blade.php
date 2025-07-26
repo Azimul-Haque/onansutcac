74cab2a41dfb28ea0bd69a82d3a46289b7bc4aa0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 19:34:14 +0600

    Commit Sat 07/26/2025 19:34:14.27 13303

---
+++ resources/views/dashboard/events/index.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h4 class="mb-3">Events List</h4>
+
+    <button class="btn btn-success mb-3" data-toggle="modal" data-target="#addModal">+ Add Event</button>
+
+    @if (session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($events as $key => $event)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $event->title }}</td>
+                    <td>{{ $event->location ?? '-' }}</td>
+                    <td>{{ $event->event_date ?? '-' }}</td>
+                    <td>
+                        <button class="btn btn-sm btn-primary"
+                            data-toggle="modal"
+                            data-target="#editModal{{ $event->id }}">Edit</button>
+
+                        <form method="POST" action="{{ route('events.destroy', $event->id) }}" class="d-inline">
+                            @csrf @method('DELETE')
+                            <button class="btn btn-sm btn-danger"
+                                onclick="return confirm('Are you sure?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+
+                {{-- Edit Modal --}}
+                <div class="modal fade" id="editModal{{ $event->id }}" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                        <form action="{{ route('events.update', $event->id) }}" method="POST">
+                            @csrf
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Edit Event</h5>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label>Title</label>
+                                        <input type="text" name="title" class="form-control" value="{{ $event->title }}" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Location</label>
+                                        <input type="text" name="location" class="form-control" value="{{ $event->location }}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Date</label>
+                                        <input type="date" name="event_date" class="form-control" value="{{ $event->event_date }}">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-success">Update</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+<!-- Add Modal -->
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <form action="{{ route('events.store') }}" method="POST">
+            @csrf
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add New Event</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Title *</label>
+                        <input type="text" name="title" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Location</label>
+                        <input type="text" name="location" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Date</label>
+                        <input type="date" name="event_date" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Add Event</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
