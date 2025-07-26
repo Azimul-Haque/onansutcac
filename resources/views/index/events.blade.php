2672f8b5b31f4091ebfbc91ff43714662ff37378
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 20:43:13 +0600

    Commit Sat 07/26/2025 20:43:13.30 28124

--- resources/views/index/events.blade.php
+++ resources/views/index/events.blade.php
@@ -55,7 +55,7 @@
                   </div>
                   <div class="portfolio2-content">
                     <span class="category">{{ $event->type }}</span>
-                    <a href="#!" title="Open Details" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><h3>AI & Sustainable Water Futures Summit</h3></a>
+                    <a href="#!" title="Open Details" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><h3>{{ $event->title }}</h3></a>
                     <p class="text-muted fst-italic mb-1">
                       <i class="bi bi-calendar-event me-2"></i> **July 15, 2025** | 9:00 AM - 4:00 PM (GMT+6)
                     </p>
