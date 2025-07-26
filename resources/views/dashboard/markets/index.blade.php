0d7f44351883f90463d8c788f11ccc153e0f2bad
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 16:59:16 +0600

    Commit Sat 07/26/2025 16:59:16.30 24304

--- resources/views/dashboard/markets/index.blade.php
+++ resources/views/dashboard/markets/index.blade.php
@@ -58,7 +58,7 @@
                         <td>
                             <a href="{{ route('index.singlemarket', $market->slug) }}" target="_blank">{{ $market->title }}</a> {{-- Changed route to index.singlemarket, and variable to $market --}}
                             <br/>
-                            <small class="badge rounded-pill bg-{{ pill_type($market->type) }}">{{ prod_type($product->type) }}</small>
+                            <small class="badge rounded-pill bg-{{ pill_type($market->type) }}">{{ ind_type($market->type) }}</small>
                         </td>
                         <td><small>{{ $market->slug }}</small></td> {{-- Changed $product->slug to $market->slug --}}
                         <td>
