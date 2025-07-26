588c103f8d75c29b18f695f3f2655090c4ed224d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 18:05:14 +0600

    Commit Sat 07/26/2025 18:05:14.26 14514

--- resources/views/index/products.blade.php
+++ resources/views/index/products.blade.php
@@ -43,12 +43,24 @@
               @endphp
             <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="{{ $delay }}">
               <div class="service-item">
-                <div class="img">
+                {{-- <div class="img">
                   @if($product->image)
                       <img src="{{ asset('images/products/' . $product->image) }}" alt="{{ $product->title }}" class="img-fluid">
                   @else
                       <img src="{{ asset('vendor/dewi/assets/img/services-1.jpg') }}" class="img-fluid" alt="">
                   @endif
+                </div> --}}
+                <div class="img position-relative"> <!-- Added position-relative here -->
+                    @if($product->image)
+                        <img src="{{ asset('images/products/' . $product->image) }}" alt="{{ $product->title }}" class="img-fluid">
+                    @else
+                        <img src="{{ asset('vendor/dewi/assets/img/services-1.jpg') }}" class="img-fluid" alt="">
+                    @endif
+
+                    <!-- Product Type Tag -->
+                    <span class="badge bg-primary position-absolute top-0 end-0 m-2" style="font-size: 0.85em; padding: 0.5em 0.75em; border-radius: 0.3rem;">
+                        {{ $product->type ?? 'Product Type' }} <!-- Replace $product->type with your actual variable -->
+                    </span>
                 </div>
                 <div class="details position-relative">
                   <div class="icon">
