c1d10614b2f73c97d73a791fef30f2f05b03630d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 18:25:40 +0600

    Commit Sat 07/26/2025 18:25:40.26 5421

--- resources/views/partials/_productsidebar.blade.php
+++ resources/views/partials/_productsidebar.blade.php
@@ -59,7 +59,7 @@
       @endif
       <div>
         <h4><a href="{{ route('index.singleproduct', $product->slug) }}">{{ $product->title }}</a></h4>
-        {{-- <time datetime="2020-01-01">{{ date('F d, Y', strtotime($product->created_at)) }}</time> --}}
+        <time datetime="2020-01-01">{{ date('F d, Y', strtotime($product->created_at)) }}</time>
       </div>
     </div><!-- End recent post item-->
     @endforeach
