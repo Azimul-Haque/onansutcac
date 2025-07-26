3081bab8d577f6438bb4e4b8f074fd86faaf2f36
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-07-26 16:45:22 +0600

    Commit Sat 07/26/2025 16:45:22.33 17622

--- database/migrations/2025_06_10_150200_create_products_table.php
+++ database/migrations/2025_06_10_150200_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();                     
+            $table->integer('type');               
             $table->string('title');               
             $table->text('slug');               
             $table->longText('text');      
