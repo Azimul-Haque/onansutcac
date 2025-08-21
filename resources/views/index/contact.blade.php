ba3ca226d08f26ce6843ec1f069cc8d1ff82e343
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 20:24:51 +0600

    Commit Thu 08/21/2025 20:24:51.25 4770

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -85,7 +85,7 @@
                   <textarea class="form-control" name="message" rows="4" placeholder="Message" required=""></textarea>
                 </div>
 
-                <img src="{{ route('captcha.image') }}" alt="Captcha Text" width="120">
+                <img src="{{ route('captcha.image') }}" alt="Captcha Text" style="height: auto; width: 120px;">
 
                 <div class="col-md-12 text-center">
 
