c589fe1b07cfd71cccb9f9eb1254280e313450d6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 14:35:28 +0600

    Commit Thu 08/21/2025 14:35:28.21 22626

--- resources/views/dashboard/teams/index.blade.php
+++ resources/views/dashboard/teams/index.blade.php
@@ -62,6 +62,7 @@
                             <small class="text-black-50">{{ $team->designation }}</small>
                         </td>
                         <td><small>{{ $team->designation }}</small></td>
+                        <td><small>{{ $team->serial }}</small></td>
                         <td>
                             @if($team->image)
                                 <img src="{{ asset('images/teams/' . $team->image) }}" alt="{{ $team->name }}" class="img-thumbnail" style="width: 50px; height: 50px; object-fit: cover;">
