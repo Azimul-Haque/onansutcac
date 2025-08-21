9fe75e49583c82f53a92deda45ac5b8e55044923
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-22 03:03:19 +0600

    Commit Fri 08/22/2025  3:03:19.27 31419

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -44,6 +44,7 @@ class IndexController extends Controller
      */
     public function __construct()
     {
+        dd(config('app.url'));
         View::share('productsforfooter', Cache::remember('products_for_footer', 60 * 24 * 30, function () {
             return Product::orderBy('id', 'desc')->get()->take(6);
         }));
