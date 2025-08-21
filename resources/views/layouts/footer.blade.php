f59960ba8f8874a83831de6829a5b5c816a6bafc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 13:33:05 +0600

    Commit Thu 08/21/2025 13:33:05.31 24633

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -82,7 +82,7 @@
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.regional-offices') }}">Global Offices</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.news') }}">Media & Press</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.success-stories') }}">Case Study</a></li>
-          <li><a href="#!">Request a Proposal</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#!">Request a Proposal</a></li>
         </ul>
       </div>
 
