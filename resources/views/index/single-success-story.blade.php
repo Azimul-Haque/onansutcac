4dd8a72f341935f50493f6924e2a322c8b1e954e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 19:45:00 +0600

    Commit Thu 08/21/2025 19:45:00.30 16667

--- resources/views/index/single-success-story.blade.php
+++ resources/views/index/single-success-story.blade.php
@@ -54,15 +54,15 @@
                 <div class="meta-bottom">
                   <i class="bi bi-folder"></i>
                   <ul class="cats">
-                    <li><a href="#" class="badge bg-secondary" style="color: #FFFFFF;">Business</a></li>
+                    <li><a href="#" class="badge bg-secondary" style="color: #FFFFFF;">{{ $successstory->type }}</a></li>
                   </ul>
 
-                  <i class="bi bi-tags"></i>
+                  {{-- <i class="bi bi-tags"></i>
                   <ul class="tags">
                     <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Creative</a></li>
                     <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Tips</a></li>
                     <li><a href="#" class="badge bg-primary" style="color: #FFFFFF;">Marketing</a></li>
-                  </ul>
+                  </ul> --}}
                 </div><!-- End meta bottom -->
 
               </article>
