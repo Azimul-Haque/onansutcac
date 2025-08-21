d6c734d88cb0d6d9ee54679bd8310ae5c91e505d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-22 02:14:57 +0600

    Commit Fri 08/22/2025  2:14:57.30 18178

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -571,6 +571,7 @@
                   <div class="swiper-slide" style="width: 371.333px; min-height: 370px; margin-right: 1px;" role="group" aria-label="{{ $key + 1 }} / 6" data-swiper-slide-index="0">
                     <div class="testimonial-item">
                       <div class="stars">
+                        $foreach($testimonial->rating)
                         <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                       </div>
                       <p>
