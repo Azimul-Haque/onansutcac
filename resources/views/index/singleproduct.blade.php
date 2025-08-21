d9140678f2887bdf607d2b32fb18e8f6b2fc7ac4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 19:51:40 +0600

    Commit Thu 08/21/2025 19:51:40.26 750

--- resources/views/index/singleproduct.blade.php
+++ resources/views/index/singleproduct.blade.php
@@ -300,44 +300,6 @@
             <!-- End testimonial item -->
             @endforeach
 
-            <div class="swiper-slide swiper-slide-prev" style="width: 371.333px; margin-right: 1px;" role="group" aria-label="2 / 5" data-swiper-slide-index="1">
-              <div class="testimonial-item">
-                <div class="post-img">
-                  <a href="{{ route('index.single-success-story', 'slug') }}" target=""><img src="{{ asset('vendor/dewi/assets/img/news/blog-post-3.webp') }}" alt="" class="img-fluid"></a>
-                </div>
-
-                <h2 class="title">
-                  <a href="{{ route('index.single-success-story', 'slug') }}">Possimus soluta ut id suscipit ea ut in quo quia et soluta</a>
-                </h2>
-                <a href="{{ route('index.single-success-story', 'slug') }}" class="newsroom-item-link" target="">Read more</a>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide swiper-slide-active" style="width: 371.333px; margin-right: 1px;" role="group" aria-label="3 / 5" data-swiper-slide-index="2">
-              <div class="testimonial-item">
-                <div class="post-img">
-                  <a href="{{ route('index.single-success-story', 'slug') }}" target=""><img src="{{ asset('vendor/dewi/assets/img/news/blog-post-3.webp') }}" alt="" class="img-fluid"></a>
-                </div>
-
-                <h2 class="title">
-                  <a href="{{ route('index.single-success-story', 'slug') }}">Possimus soluta ut id suscipit ea ut in quo quia et soluta</a>
-                </h2>
-                <a href="{{ route('index.single-success-story', 'slug') }}" class="newsroom-item-link" target="">Read more</a>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide swiper-slide-active" style="width: 371.333px; margin-right: 1px;" role="group" aria-label="3 / 5" data-swiper-slide-index="2">
-              <div class="testimonial-item">
-                <div class="post-img">
-                  <a href="{{ route('index.single-success-story', 'slug') }}" target=""><img src="{{ asset('vendor/dewi/assets/img/news/blog-post-3.webp') }}" alt="" class="img-fluid"></a>
-                </div>
-
-                <h2 class="title">
-                  <a href="{{ route('index.single-success-story', 'slug') }}">Possimus soluta ut id suscipit ea ut in quo quia et soluta</a>
-                </h2>
-                <a href="{{ route('index.single-success-story', 'slug') }}" class="newsroom-item-link" target="">Read more</a>
-              </div>
-            </div><!-- End testimonial item -->
           </div>
           <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 3" aria-current="true"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 4"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 5"></span></div>
         <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
