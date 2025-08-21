c03d60da86131b2d72b442225fb02a5b2b9c9f96
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 13:54:00 +0600

    Commit Thu 08/21/2025 13:54:00.25 30459

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -49,7 +49,7 @@
               </p>
               <ul>
                 <li><i class="bi bi-check-circle-fill"></i> <span>CoreSil™ Technology: Innovative nanoscale material used for water and textile applications.</span></li>
-                <li><i class="bi bi-check-circle-fill"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+                <li><i class="bi bi-check-circle-fill"></i> <span>Strategic Partnerships: Collaborates with organizations like NASA and Arizona State University.</span></li>
                 <li><i class="bi bi-check-circle-fill"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
               </ul>
               <p>
