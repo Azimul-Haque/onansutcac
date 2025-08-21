c3f4656086d26715823a91f35fdb4921896bd39d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 18:17:38 +0600

    Commit Thu 08/21/2025 18:17:38.28 3285

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -193,7 +193,7 @@ class IndexController extends Controller
 
     public function getSuccessStories()
     {
-        $successstoroes = Event::orderBy('id', 'desc')->get();
+        $successstoroes = Successstory::orderBy('id', 'desc')->get();
 
         return view('index.success-stories')
                         ->withSuccessstoroes($successstoroes);
