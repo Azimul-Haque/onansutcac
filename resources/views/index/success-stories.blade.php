7f308d9d52d3de3c086275abe9f8d791367566b9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 19:41:23 +0600

    Commit Thu 08/21/2025 19:41:23.23 3786

--- resources/views/index/success-stories.blade.php
+++ resources/views/index/success-stories.blade.php
@@ -58,7 +58,7 @@
       <div class="container">
         <div class="d-flex justify-content-center">
           {{ $successstories->links() }}
-          <ul>
+          {{-- <ul>
             <li><a href="#"><i class="bi bi-chevron-left"></i></a></li>
             <li><a href="#">1</a></li>
             <li><a href="#" class="active">2</a></li>
@@ -67,7 +67,7 @@
             <li>...</li>
             <li><a href="#">10</a></li>
             <li><a href="#"><i class="bi bi-chevron-right"></i></a></li>
-          </ul>
+          </ul> --}}
         </div>
       </div>
 
