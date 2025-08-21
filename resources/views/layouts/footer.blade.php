c4f7bbe3cfbe6ad6db5e52787043ae8bb1fd8d76
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 13:17:02 +0600

    Commit Thu 08/21/2025 13:17:02.23 13682

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -24,10 +24,11 @@
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.get-why-work-with-us') }}#global-presence">Global Presence</a></li>
 
 
-          <li><i class="bi bi-chevron-right"></i> <a href="https://cactusmaterials.com/" target="_blank">Cactus Materials</a></li>
+          {{-- <li><i class="bi bi-chevron-right"></i> <a href="https://cactusmaterials.com/" target="_blank">Cactus Materials</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="https://www.cactusantimicrobial.com/" target="_blank">Cactus Antimicrobial</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.terms-and-conditions') }}">Terms of Service</a></li>
-          <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.privacy-policy') }}">Privacy Policy</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.privacy-policy') }}">Privacy Policy</a></li> --}}
+          
         </ul>
       </div>
 
