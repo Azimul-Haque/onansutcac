42382d1d33302cc2913c58862dbcec295bd05f06
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 13:26:06 +0600

    Commit Thu 08/21/2025 13:26:06.26 370

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -61,7 +61,8 @@
         <ul>
           <li><i class="bi bi-chevron-right"></i> <a href="#">Product Datasheets</a></li>
           <li><i class="bi bi-chevron-right"></i> <a href="#">Testing Standards (EPA, ISO)</a></li>
-          <li><i class="bi bi-chevron-right"></i> <a href="#">Testing Standards (EPA, ISO)</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#">Technical Whitepapers</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#">Technical Whitepapers</a></li>
 
           
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.success-stories') }}">Case Study</a></li>
