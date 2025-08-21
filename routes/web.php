cf9bee0ba8515628c27233f77f298053735092ff
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 17:00:56 +0600

    Commit Thu 08/21/2025 17:00:56.27 13001

--- routes/web.php
+++ routes/web.php
@@ -99,7 +99,7 @@ Route::get('/dashboard/components', 'DashboardController@getComponents')->name('
 
 Route::get('/dashboard/abouts', 'DashboardController@getAbouts')->name('dashboard.abouts');
 Route::post('/dashboard/abouts/store', 'DashboardController@storeAbout')->name('dashboard.teams.store');
-Route::post('/dashboard/abouts/{id}/update', 'DashboardController@updateTeam')->name('dashboard.teams.update');
+Route::post('/dashboard/abouts/{id}/update', 'DashboardController@updateAbout')->name('dashboard.teams.update');
 Route::get('/dashboard/abouts/{id}/delete', 'DashboardController@deleteTeam')->name('dashboard.teams.delete');
 
 
