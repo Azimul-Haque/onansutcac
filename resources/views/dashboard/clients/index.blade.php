8fd2184ae911f9542cef1e22c68884eb9d340ff6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-22 01:25:00 +0600

    Commit Fri 08/22/2025  1:25:00.25 4405

--- resources/views/dashboard/clients/index.blade.php
+++ resources/views/dashboard/clients/index.blade.php
@@ -155,7 +155,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header bg-success">
-                    <h5 class="modal-title" id="addClientModalLabel">Add New Client</h5>
+                    <h5 class="modal-title" id="addClientModalLabel">Add New Client/ Collaborator</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
