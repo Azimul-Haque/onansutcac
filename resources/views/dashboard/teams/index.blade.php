ce6e42541c986a8679f1bb72d8f355e5441b428f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 14:30:54 +0600

    Commit Thu 08/21/2025 14:30:54.23 31596

--- resources/views/dashboard/teams/index.blade.php
+++ resources/views/dashboard/teams/index.blade.php
@@ -226,6 +226,19 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col-md-6">
+                        <div class="input-group mb-3">
+                            <input type="text"
+                                   name="serial"
+                                   class="form-control"
+                                   value="{{ old('serial') }}"
+                                   autocomplete="off"
+                                   placeholder="Team Member Designation" required>
+                            <div class="input-group-append">
+                                <div class="input-group-text"><span class="fas fa-briefcase"></span></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="form-group">
