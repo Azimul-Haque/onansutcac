3959c264045eb1c257438485326cfbea0ffb5d02
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 16:58:28 +0600

    Commit Thu 08/21/2025 16:58:28.26 9847

--- database/migrations/2025_08_21_165543_create_abouts_table.php
+++ database/migrations/2025_08_21_165543_create_abouts_table.php
@@ -15,7 +15,7 @@ class CreateAboutsTable extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();                     
-            $table->string('title');          
+            $table->string('page-location');          
             $table->Text('content');         
             $table->timestamps();      
         });
