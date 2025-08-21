aa1edd02811dbc266f4b8b07c5e8b09805ca3d07
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 16:53:33 +0600

    Commit Thu 08/21/2025 16:53:33.25 15177

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -28,7 +28,7 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('dashboard.index') }}" class="nav-link {{ Request::is('dashboard/index') ? 'active' : '' }} {{ Request::is('dashboard/index/*') ? 'active' : '' }}">
+    <a href="{{ route('dashboard.abouts') }}" class="nav-link {{ Request::is('dashboard/abouts') ? 'active' : '' }} {{ Request::is('dashboard/abouts/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-file-alt"></i>
         <p>Abouts Data</p>
     </a>
