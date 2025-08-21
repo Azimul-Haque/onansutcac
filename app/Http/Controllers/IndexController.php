f69af8fcdc3096d2e2ba09546d9d659ff48c1904
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-22 03:04:35 +0600

    Commit Fri 08/22/2025  3:04:35.28 21312

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -60,7 +60,6 @@ class IndexController extends Controller
      */
     public function index()
     {
-        dd(url('/'));
         $featuredprodandtechs = Product::where('isfeatured', 1)->get()->take(3);
         $featuredevents = Event::orderBy('id', 'desc')->get()->take(4);
         $newsforhomepage = News::orderBy('id', 'desc')->get()->take(3);
