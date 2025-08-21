b076d958e5b1dd55a1786ab50b09b712dc8b0c1e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 14:33:15 +0600

    Commit Thu 08/21/2025 14:33:15.28 8304

--- resources/views/dashboard/teams/index.blade.php
+++ resources/views/dashboard/teams/index.blade.php
@@ -227,13 +227,6 @@
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <select name="serial" id="serial" class="form-control" required>
-                            <option value="" selected disabled>Select Type</option>
-                            <option value="1">Product</option>
-                            <option value="2">Technology</option>
-                        </select>
-
-
                         <div class="input-group mb-3">
                             <input type="text"
                                    name="serial"
