3f64b11fdd7d38caa4cca73fa59723f87b1d6218
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 19:32:34 +0600

    Commit Thu 08/21/2025 19:32:34.29 3110

--- resources/views/index/singlemarket.blade.php
+++ resources/views/index/singlemarket.blade.php
@@ -50,9 +50,9 @@
 
                 <div class="meta-top">
                   <ul>
-                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('index.singlemarket', $market->slug) }}">John Doe</a></li>
+                    {{-- <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('index.singlemarket', $market->slug) }}">John Doe</a></li> --}}
                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('index.singlemarket', $market->slug) }}"><time datetime="{{ $market->created_at }}">{{ date('d F, Y', strtotime($market->created_at)) }}</time></a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="{{ route('index.singlemarket', $market->slug) }}">12 Comments</a></li>
+                    {{-- <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="{{ route('index.singlemarket', $market->slug) }}">12 Comments</a></li> --}}
                   </ul>
                 </div><!-- End meta top -->
 
