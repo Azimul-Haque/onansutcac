2ebc5d687e90ae8d09936de3f778bebf660d9000
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-22 01:25:20 +0600

    Commit Fri 08/22/2025  1:25:20.25 521

--- resources/views/dashboard/clients/index.blade.php
+++ resources/views/dashboard/clients/index.blade.php
@@ -117,7 +117,7 @@
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header bg-danger">
-                                                <h5 class="modal-title" id="deleteClientModalLabel{{ $client->id }}">Delete Client</h5>
+                                                <h5 class="modal-title" id="deleteClientModalLabel{{ $client->id }}">Delete Client/ Collaborator</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
