7154fbbfdc82c9861f1566b16f411c4d8fa1bdf6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-21 17:43:45 +0600

    Commit Thu 08/21/2025 17:43:45.28 2604

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -189,23 +189,24 @@
       <div class="container" data-aos="fade-up" data-aos-delay="100">
 
         <div class="row gy-5">
-
-          <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-            <div class="service-item">
-              <div class="img">
-                <img src="{{ asset('vendor/dewi/assets/img/services-1.jpg') }}" class="img-fluid" alt="">
-              </div>
-              <div class="details position-relative">
-                <div class="icon">
-                  <i class="bi bi-activity"></i>
+          @foreach($featuredprodandtechs as $products)
+            <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+              <div class="service-item">
+                <div class="img">
+                  <img src="{{ asset('vendor/dewi/assets/img/services-1.jpg') }}" class="img-fluid" alt="">
                 </div>
-                <a href="#!" class="stretched-link">
-                  <h3>Nesciunt Mete</h3>
-                </a>
-                <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis.</p>
+                <div class="details position-relative">
+                  <div class="icon">
+                    <i class="bi bi-activity"></i>
+                  </div>
+                  <a href="#!" class="stretched-link">
+                    <h3>Nesciunt Mete</h3>
+                  </a>
+                  <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis.</p>
+                </div>
               </div>
             </div>
-          </div><!-- End Service Item -->
+          @endforeach
 
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
             <div class="service-item">
