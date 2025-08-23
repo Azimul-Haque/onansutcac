22c86ba7a8a2cbee8ed4eabc6c90f3565a32fe06
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-23 15:05:01 +0600

    Commit Sat 08/23/2025 15:05:01.30 6797

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -42,7 +42,7 @@
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
                   <i class="bi bi-geo-alt"></i>
                   <h3>Address</h3>
-                  <p>{{ $contactdata->first() }}</p>
+                  <p>{{ $contactdata->first()->content }}</p>
                 </div>
               </div><!-- End Info Item -->
 
