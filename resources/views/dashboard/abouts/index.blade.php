5876b4b80884fec8c7bcdadef4cbd75b2a037a43
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-23 14:57:37 +0600

    Commit Sat 08/23/2025 14:57:37.32 6654

--- resources/views/dashboard/abouts/index.blade.php
+++ resources/views/dashboard/abouts/index.blade.php
@@ -72,7 +72,7 @@
                           <div class="modal-dialog modal-xl" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-primary">
-                                <h5 class="modal-title" id="editAboutModalLabel{{ $about->id }}">Update About Entry for: {{ $about->page_location }}</h5>
+                                <h5 class="modal-title" id="editAboutModalLabel{{ $about->id }}">Update About/Meta Entry for: {{ $about->page_location }}</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
