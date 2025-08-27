3d8906771d35aeeeae75cd07eb9f46f1f932aaaf
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-08-27 21:41:33 +0600

    Commit Wed 08/27/2025 21:41:33.25 24888

--- resources/views/index/markets.blade.php
+++ resources/views/index/markets.blade.php
@@ -29,7 +29,7 @@
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Industries/Projects</h2>
-        <p>Explore Our Range of Quality Offerings<br></p>
+        <p>Explore Our Range Accross The Industries<br></p>
       </div><!-- End Section Title -->
 
       <div class="container aos-init" data-aos="fade-up" data-aos-delay="100">
