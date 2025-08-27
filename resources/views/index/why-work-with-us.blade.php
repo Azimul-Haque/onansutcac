8522cdb6f251109acb6994dd7b2ed6f3a5f205dd
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-08-27 21:30:11 +0600

    Commit Wed 08/27/2025 21:30:11.27 29010

--- resources/views/index/why-work-with-us.blade.php
+++ resources/views/index/why-work-with-us.blade.php
@@ -34,21 +34,21 @@
 
         <div class="row gy-4" data-aos="fade-up">
           
-          <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
+          <div class="col-12 col-md-6">
               <div class="feature-box">
                   <i class="bi bi-lightbulb icon"></i>
                   <p class="text">Slow, controlled release of antimicrobial agents for long-lasting performance</p>
               </div>
           </div>
 
-          <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
+          <div class="col-12 col-md-6">
               <div class="feature-box">
                   <i class="bi bi-award icon"></i>
                   <p class="text">Initially developed for NASA, now available for mass market applications</p>
               </div>
           </div>
 
-          <div class="col-12 col-md-6"> <!-- Bootstrap column for responsive layout -->
+          <div class="col-12 col-md-6">
               <div class="feature-box">
                   <i class="bi bi-puzzle icon"></i>
                   <p class="text">Customizable & easy integration into a wide range of materials</p>
