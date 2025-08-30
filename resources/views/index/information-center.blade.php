b294af669c0c5cf968b23487f7c89094f5026059
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-31 00:30:24 +0600

    Commit Sun 08/31/2025  0:30:24.22 3093

--- resources/views/index/information-center.blade.php
+++ resources/views/index/information-center.blade.php
@@ -12,14 +12,14 @@
     {{-- <div class="page-title dark-background" data-aos="fade" style="background-image: url({{ asset('vendor/dewi/assets/img/page-title-bg.webp') }});"> --}}
     <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/other-pages-header-background.gif') }}');">
       <div class="container position-relative">
-        <h1>Information Center</h1>
+        {{-- <h1>Information Center</h1>
         <p>Central Hub for Knowledge & Resources</p>
         <nav class="breadcrumbs">
           <ol>
             <li><a href="{{ route('index.index') }}">Home</a></li>
             <li class="current">Information Center</li>
           </ol>
-        </nav>
+        </nav> --}}
       </div>
     </div><!-- End Page Title -->
 
