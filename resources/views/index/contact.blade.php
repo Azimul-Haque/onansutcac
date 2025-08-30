7ec3d4596bb7a38255663f0551fe76a0fcb56de7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 19:04:06 +0600

    Commit Sat 08/30/2025 19:04:06.22 21158

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -57,7 +57,7 @@
               </div><!-- End Info Item -->
               @endif
 
-              @if($contactdata[2]->content != '' || $contactdata[2]->content != null)
+              @if($contactdata[2] != '' || $contactdata[2] != null)
               <div class="col-md-6">
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="400">
                   <i class="bi bi-envelope"></i>
