2cc6a9572601b9e5d3889b65cf5df850e9f4ec54
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-31 00:20:39 +0600

    Commit Sun 08/31/2025  0:20:39.23 12838

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -21,10 +21,7 @@
           <div class="transparent-box p-5 rounded-3 shadow-lg">
               <div class="d-flex flex-column align-items-center justify-content-center text-center">
                   <h1 class="fs-1 fw-bold text-teal-200 mb-2">
-                      Transforming Industries
-                  </h1>
-                  <h2 class="fs-1 fw-bold text-teal-600">
-                      with Nano-Innovation
+                      {{ $metadata }}
                   </h2>
               </div>
           </div>
