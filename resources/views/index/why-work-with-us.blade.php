53652b97fbba189f424668261db1fe35f0530dce
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 21:01:05 +0600

    Commit Sat 08/30/2025 21:01:05.25 5795

--- resources/views/index/why-work-with-us.blade.php
+++ resources/views/index/why-work-with-us.blade.php
@@ -456,34 +456,27 @@
 
 @section('third_party_scripts')
 
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script>
-      // Initialize map
-      var map = L.map('map').setView([20, 0], 2);
+    <!-- Leaflet JS -->
+      <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-      // Add tiles
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-      }).addTo(map);
+      <script>
+        // Initialize map
+        var map = L.map('map').setView([20, 0], 2);
 
-      // Define custom icon (blue pin)
-      var customIcon = L.icon({
-        iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // pin image
-        iconSize: [32, 32], // size of the icon
-        iconAnchor: [16, 32], // point of the icon at marker's location
-        popupAnchor: [0, -32] // position of popup relative to icon
-      });
+        // Load OpenStreetMap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-      // Add markers with custom icons
-      L.marker([1.3521, 103.8198], {icon: customIcon}).addTo(map)
-        .bindPopup('<b>Singapore</b>');
+        // Add markers
+        L.marker([40.7128, -74.0060]).addTo(map) // New York
+          .bindPopup("<b>New York, USA</b>").openPopup();
 
-      L.marker([40.7128, -74.0060], {icon: customIcon}).addTo(map)
-        .bindPopup('<b>New York, USA</b>');
+        L.marker([1.3521, 103.8198]).addTo(map) // Singapore
+          .bindPopup("<b>Singapore</b>");
 
-      L.marker([51.5074, -0.1278], {icon: customIcon}).addTo(map)
-        .bindPopup('<b>London, UK</b>');
-    </script>
+        L.marker([51.5074, -0.1278]).addTo(map) // London
+          .bindPopup("<b>London, UK</b>");
+      </script>
 
 @endsection
