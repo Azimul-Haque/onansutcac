2eea6effc20cadd02cdf1fab9708064717624d04
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 20:59:06 +0600

    Commit Sat 08/30/2025 20:59:06.21 25414

--- resources/views/index/why-work-with-us.blade.php
+++ resources/views/index/why-work-with-us.blade.php
@@ -436,10 +436,10 @@
         <div id="map"></div>
       </div>
         {{-- <div class="row g-4 justify-content-center mb-4">
-            <!-- Vision Statement Column -->
+            
             <div class="col-12 col-md-12">
               <div id="map"></div>
-              {{-- <img src="{{ asset('images/global-presence.png') }}" class="img-fluid rounded-4 mt-4" alt="Global Presence" style="width: 100%;"> --}}
+              <img src="{{ asset('images/global-presence.png') }}" class="img-fluid rounded-4 mt-4" alt="Global Presence" style="width: 100%;">
             </div>
         </div> --}}
       </div>
