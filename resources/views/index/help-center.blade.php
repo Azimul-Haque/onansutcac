d84d6c7cb117a93bd5fba4117d516289af9ec27c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 22:20:57 +0600

    Commit Sat 08/30/2025 22:20:57.22 27215

--- resources/views/index/help-center.blade.php
+++ resources/views/index/help-center.blade.php
@@ -19,14 +19,14 @@
     {{-- <div class="page-title dark-background" data-aos="fade" style="background-image: url({{ asset('vendor/dewi/assets/img/page-title-bg.webp') }});"> --}}
     <div class="page-title dark-background" data-aos="fade" style="background-image: url('{{ asset('images/other-pages-header-background.gif') }}');">
       <div class="container position-relative">
-        <h1>Help Center-FAQ</h1>
+        {{-- <h1>Help Center-FAQ</h1>
         <p>Answers at Your Fingertips</p>
         <nav class="breadcrumbs">
           <ol>
             <li><a href="{{ route('index.index') }}">Home</a></li>
             <li class="current">Help Center-FAQ</li>
           </ol>
-        </nav>
+        </nav> --}}
       </div>
     </div><!-- End Page Title -->
 
