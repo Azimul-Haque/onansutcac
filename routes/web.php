7f757c432b67c4986d4b190f78671cb65bf14a14
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 21:33:27 +0600

    Commit Sat 08/30/2025 21:33:27.32 28826

--- routes/web.php
+++ routes/web.php
@@ -155,10 +155,10 @@ Route::post('/dashboard/testimonials/{id}/update', 'DashboardController@updateTe
 Route::get('/dashboard/testimonials/{id}/delete', 'DashboardController@deleteTestimonial')->name('dashboard.testimonials.delete');
 
 // --- Global Presence Routes ---
-Route::get('/dashboard/global-presence', 'DashboardController@getGlobal-Presence')->name('dashboard.global-presence');
-Route::post('/dashboard/global-presence/store', 'DashboardController@storeGlobal-Presence')->name('dashboard.global-presence.store');
-Route::post('/dashboard/global-presence/{id}/update', 'DashboardController@updateGlobal-Presence')->name('dashboard.global-presence.update');
-Route::get('/dashboard/global-presence/{id}/delete', 'DashboardController@deleteGlobal-Presence')->name('dashboard.global-presence.delete');
+Route::get('/dashboard/global-presence', 'DashboardController@getGlobalPresence')->name('dashboard.global-presence');
+Route::post('/dashboard/global-presence/store', 'DashboardController@storeGlobalPresence')->name('dashboard.global-presence.store');
+Route::post('/dashboard/global-presence/{id}/update', 'DashboardController@updateGlobalPresence')->name('dashboard.global-presence.update');
+Route::get('/dashboard/global-presence/{id}/delete', 'DashboardController@deleteGlobalPresence')->name('dashboard.global-presence.delete');
 
 // --- Information Center Routes ---
 Route::get('/dashboard/information-center', 'DashboardController@getInformationCenter')->name('dashboard.information-center');
