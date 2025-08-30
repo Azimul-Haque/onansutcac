a213a6eda2113ebccd53b980a260ab6d41bd17ca
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-31 00:26:23 +0600

    Commit Sun 08/31/2025  0:26:23.26 19638

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -175,7 +175,7 @@
     </section> --}}
 
     <!-- Services Section -->
-    <section id="services" class="services section">
+    <section id="services" class="services section light-background">
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
