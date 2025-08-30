2c025f0fc45fc3dbb0d5892d9dbccc851f86f063
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 19:42:54 +0600

    Commit Sat 08/30/2025 19:42:54.31 5145

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -434,7 +434,7 @@ class DashboardController extends Controller
         $team->name = $request->name;
         $team->designation = $request->designation;
         $team->serial = $request->serial;
-        $team->about = Purifier::clean($request->about, 'youtube');
+        $team->about = $request->about;
 
         if($request->hasFile('image')) {
             if ($team->image && file_exists(public_path('images/teams/' . $team->image))) {
