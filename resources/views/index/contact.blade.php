b9995bcdb7adfed646f812bb5edcdd8af9b26bd4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 19:07:46 +0600

    Commit Sat 08/30/2025 19:07:46.23 27435

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -37,7 +37,7 @@
         <div class="row gy-4">
           <div class="col-lg-6 ">
             <div class="row gy-4">
-              @if($contactdata[0]->content != '')
+              @if(strlen( strip_tags($contactdata[0]->content)) > 0)
               <div class="col-lg-12">
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
                   <i class="bi bi-geo-alt"></i>
