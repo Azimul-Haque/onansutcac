5af631416122cc0bd19a8d2ba9b83adddf7e5374
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 18:54:27 +0600

    Commit Sat 08/30/2025 18:54:27.33 17432

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -52,7 +52,7 @@
         <h4>Sustainability</h4>
         <ul>
           <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.sdg-alignment') }}">SDG Alignment</a></li>
-          <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.sdg-impact') }}">Social Impact</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="{{ route('index.social-impact') }}">Social Impact</a></li>
         </ul>
       </div>
 
