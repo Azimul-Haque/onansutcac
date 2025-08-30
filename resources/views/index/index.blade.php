e8e04a0bd8e619bb2366581618b4d28fe88dca61
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-31 00:22:50 +0600

    Commit Sun 08/31/2025  0:22:50.23 13788

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -36,7 +36,7 @@
               transform: scale(1.05);
           }
         </style>
-        <p data-aos="fade-up" data-aos-delay="200"><small>{{ $metadata[9]->content }}</small></p>
+        <p data-aos="fade-up" data-aos-delay="200"><small>{{ strptags($metadata[9]->content) }}</small></p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
           {{-- <a href="#about" class="btn-get-started">Get Started</a>
           <a href="https://www.youtube.com/watch?v=U6fC4Ij608A" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
