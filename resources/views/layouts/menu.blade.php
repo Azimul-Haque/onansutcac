a9bc1a5aba8ae35c741d3066749b22335cd2229a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 21:32:15 +0600

    Commit Sat 08/30/2025 21:32:15.27 15133

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -77,6 +77,14 @@
 </li>
 
 <li class="nav-item">
+    <a href="#!" class="nav-link {{ Request::is('dashboard/global-presence') ? 'active' : '' }} {{ Request::is('dashboard/information-center/*') ? 'active' : '' }}">
+    {{-- <a href="{{ route('dashboard.information-center') }}" class="nav-link {{ Request::is('dashboard/information-center') ? 'active' : '' }} {{ Request::is('dashboard/information-center/*') ? 'active' : '' }}"> --}}
+        <i class="nav-icon fas fa-info-circle"></i>
+        <p>Information Center</p>
+    </a>
+</li>
+
+<li class="nav-item">
     <a href="#!" class="nav-link {{ Request::is('dashboard/information-center') ? 'active' : '' }} {{ Request::is('dashboard/information-center/*') ? 'active' : '' }}">
     {{-- <a href="{{ route('dashboard.information-center') }}" class="nav-link {{ Request::is('dashboard/information-center') ? 'active' : '' }} {{ Request::is('dashboard/information-center/*') ? 'active' : '' }}"> --}}
         <i class="nav-icon fas fa-info-circle"></i>
