a2743afc3548bb9f4549fa91ed73830b785dd25a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-08-30 20:36:10 +0600

    Commit Sat 08/30/2025 20:36:10.28 3787

--- resources/views/dashboard/abouts/index.blade.php
+++ resources/views/dashboard/abouts/index.blade.php
@@ -69,7 +69,7 @@
                         </td>
 
                         <div class="modal fade" id="editAboutModal{{ $about->id }}" tabindex="-1" role="dialog" aria-labelledby="editAboutModalLabel{{ $about->id }}" aria-hidden="true" data-backdrop="static">
-                          <div class="modal-dialog modal-xl" role="document">
+                          <div class="modal-dialog modal-lg" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-primary">
                                 <h5 class="modal-title" id="editAboutModalLabel{{ $about->id }}">Update About/Meta Entry for: {{ $about->page_location }}</h5>
@@ -110,7 +110,7 @@
                           </div>
                         </div>
                         <div class="modal fade" id="deleteAboutModal{{ $about->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteAboutModalLabel{{ $about->id }}" aria-hidden="true" data-backdrop="static">
-                          <div class="modal-dialog modal-xl" role="document">
+                          <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
                                 <h5 class="modal-title" id="deleteAboutModalLabel{{ $about->id }}">Delete About Entry</h5>
