c98ca64f92fce8c140e5a14ce447938239ff381b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-09-05 19:17:19 +0600

    Commit Fri 09/05/2025 19:17:19.25 6903

--- database/migrations/2025_08_30_212446_create_globalpresences_table.php
+++ database/migrations/2025_08_30_212446_create_globalpresences_table.php
@@ -16,6 +16,7 @@ class CreateGlobalpresencesTable extends Migration
         Schema::create('globalpresences', function (Blueprint $table) {
             $table->id();
             $table->string('placename');
+            $table->string('address');
             $table->text('locationurl')->nullable();
             $table->decimal('lat', 10, 7)->nullable();
             $table->decimal('lng', 10, 7)->nullable();
