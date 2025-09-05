6b381cf4cee8254c2d520bce33c565cd9fecdb08
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-09-05 15:47:11 +0600

    Commit Fri 09/05/2025 15:47:11.28 12659

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -132,7 +132,7 @@
                                             </div>
                                         </div>
 
-                                        <div>
+                                        <div class="">
                                             <div class="col-md-6">
                                                 <div class="input-group mb-3">
                                                     <input type="text"
