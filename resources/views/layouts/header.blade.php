dcbb032506942aac3c9ae77684834ddc785306ce
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-09-19 09:34:49 +0600

    Commit Fri 09/19/2025  9:34:49.31 4812

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -85,7 +85,7 @@
                   }
                 
                 .navbar-nav .dropdown-item.indented {
-                    padding-left: 2.5rem; /* Add more padding for indentation */
+                    padding-left: 3.5rem; /* Add more padding for indentation */
                 }
               </style>
               {{-- <ul>
