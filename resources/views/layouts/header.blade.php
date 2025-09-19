78550ff34b5898ea925b72edbc1222ef7b4c8a32
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-09-19 09:07:50 +0600

    Commit Fri 09/19/2025  9:07:50.22 15904

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -61,9 +61,7 @@
                 </li>
                 <li class="dropdown"><a href="#!">Corrosion</a>
                   <ul>
-                    <li><a href="{{ url('/') }}/products/coresil-additive">CoreSil&trade; Textile</a></li>
-                    <li><a href="{{ url('/') }}/products/coresil-additive">CoreSil&trade; Polymers</a></li>
-                    <li><a href="{{ url('/') }}/products/coresil-additive">CoreSil&trade; Metals</a></li>
+                    <li><a href="{{ url('/') }}/products/coresil-additive">Cera2D&trade; Corrosion inhibitor</a></li>
                   </ul>
                 </li>
                 <li><a href="#!">PFAS</a></li>
