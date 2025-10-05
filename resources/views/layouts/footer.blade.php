19acd17123759592da6745d8327624dfca94970e
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-10-05 15:48:16 +0600

    Commit Sun 10/05/2025 15:48:16.26 16567

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,41 +42,36 @@
         <ul style="list-style: none; padding-left: 0;">
           <!-- 1. Biofouling Category -->
           <li style="margin-top: 15px;">
-              <span style="display: flex; align-items: center;">
-                  <i class="bi bi-chevron-right" style="margin-right: 5px; font-size: 14px;"></i> 
-                  <strong>Biofouling:</strong>
-              </span>
+              <!-- Removed chevron and kept only the strong category title -->
+              <strong>Biofouling:</strong>
               
               <!-- Nested list for CoreSil™ products, double indented -->
-              <ul style="list-style: none; padding-left: 20px; margin-top: 5px;">
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/revolutionizing-ro-membrane-with-nano-technology">CoreSil™ RO Membrane</a></li>
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/coresil-additive">CoreSil™ RO Additives</a></li>
-                  <!-- Additional products below CoreSil Additives -->
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="#">CoreSil™ Textile</a></li>
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="#">CoreSil™ Polymers</a></li>
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="#">CoreSil™ Metals</a></li>
+              <ul style="list-style: none; padding-left: 0; margin-top: 5px;">
+                  <!-- Product links now directly follow list items, no chevrons -->
+                  <li style="padding-left: 20px;"><a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/revolutionizing-ro-membrane-with-nano-technology">CoreSil™ RO Membrane</a></li>
+                  <li style="padding-left: 20px;"><a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/coresil-additive">CoreSil™ RO Additives</a></li>
+                  <!-- Further nested products for Textile, Polymers, Metals -->
+                  <li style="padding-left: 40px;"><a style="color: white; text-decoration: none;" href="#">CoreSil™ Textile</a></li>
+                  <li style="padding-left: 40px;"><a style="color: white; text-decoration: none;" href="#">CoreSil™ Polymers</a></li>
+                  <li style="padding-left: 40px;"><a style="color: white; text-decoration: none;" href="#">CoreSil™ Metals</a></li>
               </ul>
           </li>
           
           <li style="margin-top: 25px;">
               <!-- 2. Corrosion Category -->
-              <span style="display: flex; align-items: center;">
-                  <i class="bi bi-chevron-right" style="margin-right: 5px; font-size: 14px;"></i> 
-                  <strong>Corrosion:</strong>
-              </span>
-              <ul style="list-style: none; padding-left: 20px; margin-top: 5px;">
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/corrosion-inhibitor">Cera 2D™ Corrosion Inhibitor</a></li>
+              <strong>Corrosion:</strong>
+              <ul style="list-style: none; padding-left: 0; margin-top: 5px;">
+                  <!-- Product links now directly follow list items, no chevrons -->
+                  <li style="padding-left: 20px;"><a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/corrosion-inhibitor">Cera 2D™ Corrosion Inhibitor</a></li>
               </ul>
           </li>
           
           <li style="margin-top: 25px;">
               <!-- 3. PFAS Category -->
-              <span style="display: flex; align-items: center;">
-                  <i class="bi bi-chevron-right" style="margin-right: 5px; font-size: 14px;"></i> 
-                  <strong>PFAS:</strong>
-              </span>
-              <ul style="list-style: none; padding-left: 20px; margin-top: 5px;">
-                  <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/endpfas">EndPFAS™</a></li>
+              <strong>PFAS:</strong>
+              <ul style="list-style: none; padding-left: 0; margin-top: 5px;">
+                  <!-- Product links now directly follow list items, no chevrons -->
+                  <li style="padding-left: 20px;"><a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/endpfas">EndPFAS™</a></li>
               </ul>
           </li>
       </ul>
