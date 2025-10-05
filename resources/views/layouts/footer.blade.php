700b665b6852b129efb41d56972b782498777e7d
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-10-05 15:50:21 +0600

    Commit Sun 10/05/2025 15:50:21.25 31319

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -41,7 +41,7 @@
         </ul> -->
         <ul style="list-style: none; padding-left: 0;">
           <strong>Biofouling:</strong>
-          <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a style="color: white; text-decoration: none;" href="{{ route('index.index') }}/products/revolutionizing-ro-membrane-with-nano-technology">CoreSil™ RO Membrane</a></li>
+          <li><i class="bi bi-chevron-right" style="margin-right: 5px;"></i> <a href="{{ route('index.index') }}/products/revolutionizing-ro-membrane-with-nano-technology">CoreSil™ RO Membrane</a></li>
           <!-- 1. Biofouling Category -->
           <li style="margin-top: 15px;">
               <span style="display: flex; align-items: center;">
